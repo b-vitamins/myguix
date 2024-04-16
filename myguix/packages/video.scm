92d72e192b1973d46851416bad85f8e374b7d5ab
Ayan Das <bvits@riseup.net>
2024-04-17 04:02:37 +0530

    docs: improve documentation

--- myguix/packages/video.scm
+++ myguix/packages/video.scm
@@ -22,23 +22,24 @@
     (version "0.6.0")
     (source
      (origin
-      (method git-fetch)
-      (uri (git-reference
-            (url "https://github.com/dmlc/decord")
-            (commit (string-append "v" version))
-            (recursive? #t)))
-      (file-name (git-file-name name version))
-      (sha256
-       (base32 "1wh9bg3m1bnxqzqmm1pg3hlfhlvb3xgcxyf0qw8i9j7mv6z4xqqr"))))
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/dmlc/decord")
+             (commit (string-append "v" version))
+             (recursive? #t)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1wh9bg3m1bnxqzqmm1pg3hlfhlvb3xgcxyf0qw8i9j7mv6z4xqqr"))))
     (build-system cmake-build-system)
     (arguments
      `(#:tests? #f
-       #:configure-flags
-       (list "-DUSE_CUDA=0" "-DCMAKE_BUILD_TYPE=Release")))
+       #:configure-flags (list "-DUSE_CUDA=0" "-DCMAKE_BUILD_TYPE=Release")))
     (native-inputs (list ffmpeg-4 python pkg-config))
     (home-page "https://github.com/dmlc/decord")
-    (synopsis "@code{Decord} is a reverse procedure of @code{Record}. It provides convenient video slicing methods based on a thin wrapper on top of hardware accelerated video decoders, e.g. 1) FFMPEG/LibAV, 2) NVIDEA Codecs, 3) Intel Codecs")
-    (description "@code{Decord} was designed to handle awkward video shuffling experience in order to provide smooth experiences similar to random image loader for deep learning.
+    (synopsis
+     "@code{Decord} is a reverse procedure of @code{Record}. It provides convenient video slicing methods based on a thin wrapper on top of hardware accelerated video decoders, e.g. 1) FFMPEG/LibAV, 2) NVIDEA Codecs, 3) Intel Codecs")
+    (description
+     "@code{Decord} was designed to handle awkward video shuffling experience in order to provide smooth experiences similar to random image loader for deep learning.
 
 @code{Decord} is also able to decode audio from both video and audio files. One can slice video and audio together to get a synchronized result; hence providing a one-stop solution for both video and audio decoding.")
     (license license:asl2.0)))
