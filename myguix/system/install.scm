e51f4c4a564ed82c05e4e19b45767583261810db
Ayan Das <bvits@riseup.net>
2024-05-05 17:47:43 +0530

    Add new channel module system with image creation script install.scm
    - Introduced `channels.tmpl` for customized channel management, setting up `guix`, `myguix`, `nonguix`, and `guix-science` channels with their respective URLs and OpenPGP keys.
    - Created `install.scm` to define a custom Guix system installation OS tailored to specific hardware and user requirements, including kernel, firmware, packages, and services.
    - Added public OpenPGP keys for `bordeaux.guix.gnu.org`, `ci.guix.gnu.org`, and `substitutes.nonguix.org` to ensure secure software substitution and package updates.

---
+++ myguix/system/install.scm
@@ -0,0 +1,56 @@
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+;;; Copyright © 2019 Alex Griffin <a@ajgrf.com>
+;;; Copyright © 2019 Pierre Neidhardt <mail@ambrevar.xyz>
+;;; Copyright © 2022 Jonathan Brielmaier <jonathan.brielmaier@web.de>
+;;; Copyright © 2024 Hilton Chain <hako@ultrarare.space>
+;;; Copyright © 2024 Ayan Das <ayan@iisc.ac.in>
+;; Generate a bookable image (e.g. for USB sticks, etc.) with:
+;; $ guix system image --image-type=iso9660 myguix/system/install.scm
+(define-module (myguix system install)
+  #:use-module (guix channels)
+  #:use-module (guix gexp)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages curl)
+  #:use-module (gnu packages emacs)
+  #:use-module (gnu packages package-management)
+  #:use-module (gnu packages version-control)
+  #:use-module (gnu services)
+  #:use-module (gnu services base)
+  #:use-module (gnu system)
+  #:use-module (gnu system install)
+  #:use-module (nongnu packages linux)
+  #:export (my-installation-os))
+
+(define my-installation-os
+  (operating-system
+    (inherit installation-os)
+    (kernel linux)
+    (kernel-loadable-modules (list broadcom-sta))
+    (kernel-arguments '("modprobe.blacklist=b43,b43legacy,ssb,bcm43xx,brcm80211,brcmfmac,brcmsmac,bcma"))
+    (firmware (list linux-firmware broadcom-bt-firmware iwlwifi-firmware))
+    (packages (append (list git curl coreutils emacs-no-x-toolkit)
+                      (operating-system-packages installation-os)))
+    (services
+     (cons* (simple-service 'channel-file etc-service-type
+                            (list `("channels.scm" ,(local-file
+                                                     "examples/channels.tmpl"))))
+            (modify-services (operating-system-user-services installation-os)
+              (guix-service-type config =>
+                                 (guix-configuration (inherit config)
+                                                     (discover? #t)
+                                                     (authorized-keys (append (list
+                                                                               (local-file
+                                                                                "keys/ci.guix.gnu.org.pub")
+                                                                               
+                                                                               (local-file
+                                                                                "keys/substitutes.nonguix.org.pub")
+                                                                               
+                                                                               (local-file
+                                                                                "keys/bordeaux.guix.gnu.org.pub"))
+                                                                       %default-authorized-guix-keys))
+                                                     (substitute-urls (append (list
+                                                                               "https://ci.guix.gnu.org.pub"
+                                                                               "https://substitutes.nonguix.org.pub"
+                                                                               "https://bordeaux.guix.gnu.org.pub"))))))))))
+
+my-installation-os
