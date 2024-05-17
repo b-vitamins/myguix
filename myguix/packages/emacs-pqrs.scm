661c7362787d75c63e08a45ee22946b0e8e2219d
Ayan Das <bvits@riseup.net>
2024-05-17 14:03:52 +0530

    add package arXiv mode

--- myguix/packages/emacs-pqrs.scm
+++ myguix/packages/emacs-pqrs.scm
@@ -218,3 +218,31 @@
     (description
      "An Emacs Lisp library for interacting with a Haskell language server such as @code{haskell-language-server} using Microsoft's Language Server Protocol. The library acts as a client for @code{lsp-mode}.")
     (license license:gpl3+)))
+
+(define-public emacs-arxiv-mode
+  (package
+    (name "emacs-arxiv-mode")
+    (version "0.4.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/fizban007/arxiv-mode")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0dbsi5343zw7dn6qq8226iamb12nma8k9lqbvd9qlhq8k62pswas"))))
+    (build-system emacs-build-system)
+    (inputs (list emacs-hydra))
+    (home-page "https://github.com/fizban007/arxiv-mode")
+    (synopsis
+     "arxiv-mode is an Emacs major mode for viewing updates on arXiv.org.")
+    (description
+     "@code{arxiv-mode} provides many functions for accessing arXiv.org. To browse the daily new submissions list in a category, run @code{M-x arxiv-read-new}. To browse the recent (weekly) submissions, run @code{M-x arxiv-read-recent}. Use @{M-x arxiv-read-author} to search for specific author(s). Use @code{M-x arxiv-search} to perform a simple search on the arXiv database.
+
+For more complicated searches, use @code{M-x arxiv-complex-search}. This command allows user to dynamically refine and modify search conditions. You can also use `r' to refine search condition in the abstract list obtained from a search.
+
+In the article list, use `n' and `p' to navigate the article list. Press `SPC' to toggle visibility of the abstract window. Press `RET' to open the entry in a web browser. Press `d' to download the pdf. Press `b' to export the bibtex entry of current paper to your specified .bib file. Press B to export the bibtex entry to a new buffer. Press `e' to download pdf and add a bibtex entry with a link to the actual pdf file.
+
+All available commands are listed in a hydra help menu accessible by pressing `?' whenever you are in the article list.")
+    (license license:gpl3+)))
