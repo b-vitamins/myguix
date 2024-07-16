34dc1939db5c39945f7a036be3cd3a5cf9c55644
Ayan Das <bvits@riseup.net>
2024-07-16 21:33:30 +0530

    feat: Add package pthreadpool-4fe0e1 to module parallel.scm
    - Define new package `pthreadpool @ 4fe0e1' as dep of `pytorch @ 2.3.1'
    - Stream module imports
    - Replace `googletest' with `googletest-e2239e' as inputs for `cpuinfo'

--- myguix/packages/parallel.scm
+++ myguix/packages/parallel.scm
@@ -1,7 +1,11 @@
 (define-module (myguix packages parallel)
-  #:use-module (gnu packages parallel)
+  #:use-module (myguix packages maths)
+  #:use-module (myguix packages check)
+  #:use-module (guix packages)
   #:use-module (guix git-download)
-  #:use-module (guix packages))
+  #:use-module (gnu packages)
+  #:use-module (gnu packages parallel)
+  #:use-module (gnu packages check))
 
 (define-public cpuinfo-3c8b15
   (let ((version "0.0")
@@ -18,4 +22,26 @@
                 (file-name (git-file-name name version))
                 (sha256
                  (base32
-                  "1c3hqrncha4q956x43yqpy7w3szckwjxbh2gbhs9kfx2c4g6ij3s")))))))
+                  "1c3hqrncha4q956x43yqpy7w3szckwjxbh2gbhs9kfx2c4g6ij3s"))))
+      (inputs
+       (list googletest-e2239e googlebenchmark)))))
+
+(define-public pthreadpool-4fe0e1
+  (let ((commit "4fe0e1e183925bf8cfa6aae24237e724a96479b8")
+        (version "0.1")
+        (revision "3"))
+    (package
+			(inherit pthreadpool)
+      (name "pthreadpool")
+      (version (git-version version revision commit))
+      (home-page "https://github.com/Maratyszcza/pthreadpool")
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference (url home-page) (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "0qcz27w1wgzsag5shwkh686rf6jpilwplgqb0fjb84446lvjd1j7"))
+                (patches (search-patches "pthreadpool-system-libraries-rev3.patch"))))
+      (inputs
+       (list googletest-e2239e googlebenchmark fxdiv-b408327)))))
