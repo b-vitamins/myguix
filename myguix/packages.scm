69fdc90e0a453ff075260cff56ee572eb7661cef
Ayan Das <bvits@riseup.net>
2024-07-16 15:03:14 +0530

    feat: add myguix/packages.scm
    - Create myguix/packages.scm
    - Add `%patch-path parameter' for the channel
    - Copy the `search-patch' function from gnu/packages.scm

---
+++ myguix/packages.scm
@@ -0,0 +1,15 @@
+(define-module (myguix packages)
+  #:use-module (guix ui)
+  #:use-module (guix diagnostics))
+
+(define %patch-path
+  (make-parameter
+   (map (lambda (directory)
+          (string-append directory "/myguix/packages/patches"))
+        %load-path)))
+
+(define (search-patch file-name)
+  "Search the patch FILE-NAME.  Raise an error if not found."
+  (or (search-path (%patch-path) file-name)
+      (raise (formatted-message (G_ "~a: patch not found")
+                                file-name))))
