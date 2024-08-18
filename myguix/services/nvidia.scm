c53ba53ecd53e4b6816d1b03e0e10492aa58f96a
Ayan Das <bvits@riseup.net>
2024-08-19 01:45:31 +0530

    feat: Add nvidia services module from nonguix to myguix
    - Imported and renamed the nvidia services file (provides nvidia-service-type) from nonguix to myguix.
    - This commit is part of the effort to remove dependency on third-party channels.

---
+++ myguix/services/nvidia.scm
@@ -0,0 +1,65 @@
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+;;; Copyright © 2022, 2024 Hilton Chain <hako@ultrarare.space>
+;;; Copyright © 2024 Ayan Das <bvits@riseup.net>
+
+(define-module (myguix services nvidia)
+  #:use-module (guix gexp)
+  #:use-module (guix records)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu services)
+  #:use-module (gnu services base)
+  #:use-module (gnu services linux)
+  #:use-module (gnu services shepherd)
+  #:use-module (myguix packages nvidia)
+  #:export (nvidia-configuration
+            nvidia-configuration?
+            nvidia-configuration-record?
+            nvidia-service-type))
+
+(define-record-type* <nvidia-configuration>
+  nvidia-configuration make-nvidia-configuration
+  nvidia-configuration?
+  (driver   nvidia-configuration-driver
+            (default nvda))             ; file-like
+  (firmware nvidia-configuration-firmware
+            (default nvidia-firmware))  ; file-like
+  (module   nvidia-configuration-module
+            (default nvidia-module)))   ; file-like
+
+(define (nvidia-shepherd-service config)
+  (let ((nvidia-driver (nvidia-configuration-driver config))
+        (nvidia-smi (file-append nvidia-driver "/bin/nvidia-smi")))
+    (list (shepherd-service
+           (documentation "Prepare system environment for NVIDIA driver.")
+           (provision '(nvidia))
+           (requirement '(udev))
+           (one-shot? #t)
+           (modules '(((guix build utils) #:select (invoke/quiet))
+                      ((rnrs io ports) #:select (get-line))))
+           (start
+            #~(lambda _
+                (when (file-exists? "/proc/driver/nvidia")
+                  (let ((modprobe (call-with-input-file
+                                      "/proc/sys/kernel/modprobe" get-line)))
+                    (false-if-exception
+                     (begin
+                       (invoke/quiet modprobe "--" "nvidia_uvm")
+                       (invoke/quiet #$nvidia-smi)))))))))))
+
+(define nvidia-service-type
+  (service-type
+   (name 'nvidia)
+   (extensions
+    (list (service-extension shepherd-root-service-type
+                             nvidia-shepherd-service)
+          (service-extension profile-service-type
+                             (compose list nvidia-configuration-driver))
+          (service-extension udev-service-type
+                             (compose list nvidia-configuration-driver))
+          (service-extension firmware-service-type
+                             (compose list nvidia-configuration-firmware))
+          (service-extension linux-loadable-module-service-type
+                             (compose list nvidia-configuration-module))))
+   (default-value (nvidia-configuration))
+   (description "Prepare system environment for NVIDIA driver.")))
