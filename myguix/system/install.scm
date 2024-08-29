abd9003103ae5e40183bd532cfa077a3a00fcc5d
Ayan Das <bvits@riseup.net>
2024-08-29 05:56:57 +0530

    system: install: update `my-installation-os`
    - `my-installation-os` images will now:
      - use kernel `linux-6.10`,
      - have removed needless `kernel-arguments`
      - use `linux-firmware` and `sof-firmware`
      - have `ctrl:nocaps` and `altwin:swap_alt_win` `keyboard-layout` toggles
    
    - We also define and export
      a) `%my-channels`,
      b) `%my-authorized-keys`,
      c) `%my-substitute-urls`,
      d) `my-installation-os`.

--- myguix/system/install.scm
+++ myguix/system/install.scm
@@ -10,8 +10,7 @@
 ;; $ guix system image --image-type=iso9660 myguix/system/install.scm
 
 (define-module (myguix system install)
-  #:use-module (guix channels)
-  #:use-module (guix gexp)
+  #:use-module (gnu)
   #:use-module (gnu packages base)
   #:use-module (gnu packages curl)
   #:use-module (gnu packages emacs)
@@ -21,36 +20,60 @@
   #:use-module (gnu services base)
   #:use-module (gnu system)
   #:use-module (gnu system install)
+  #:use-module (guix channels)
+  #:use-module (guix gexp)
   #:use-module (myguix packages linux)
-  #:export (my-installation-os))
+  #:export (%my-channels %my-authorized-keys %my-substitute-urls
+                         my-installation-os))
+
+(define %my-channels
+  (list (channel
+          (name 'guix)
+          (url "https://git.savannah.gnu.org/git/guix.git")
+          (branch "master")
+          (introduction
+           (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+             "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+        (channel
+          (name 'myguix)
+          (url "https://github.com/b-vitamins/myguix.git")
+          (branch "master")
+          (introduction
+           (make-channel-introduction
+            "85d58b09dc71e9dc9834b666b658f79d2e212d65"
+            (openpgp-fingerprint
+             "883B CA6B D275 A5F2 673C  C5DD 2AD3 2FC0 2A50 01F7"))))))
+
+(define %my-authorized-keys
+  (append (list (local-file "keys/substitutes.myguix.bvits.in.pub"))
+          %default-authorized-guix-keys))
+
+(define %my-substitute-urls
+  (append (list "https://substitutes.myguix.bvits.in")
+          %default-substitute-urls))
 
 (define my-installation-os
   (operating-system
     (inherit installation-os)
-    (kernel linux)
-    (kernel-loadable-modules (list broadcom-sta))
-    (kernel-arguments '("modprobe.blacklist=b43,b43legacy,ssb,bcm43xx,brcm80211,brcmfmac,brcmsmac,bcma"))
-    (firmware (list linux-firmware broadcom-bt-firmware iwlwifi-firmware))
+    (kernel linux-6.10)
+    (firmware (list linux-firmware sof-firmware))
+    (keyboard-layout (keyboard-layout "us" "altgr-intl"
+                                      #:options '("ctrl:nocaps"
+                                                  "altwin:swap_alt_win")))
     (packages (append (list git curl coreutils emacs-no-x-toolkit)
                       (operating-system-packages installation-os)))
     (services
-     (cons* (simple-service 'channel-file etc-service-type
-                            (list `("channels.scm" ,(local-file
-                                                     "examples/channels.tmpl"))))
-            (modify-services (operating-system-user-services installation-os)
-              (guix-service-type config =>
-                                 (guix-configuration (inherit config)
-                                                     (discover? #t)
-                                                     (authorized-keys (append (list
-                                                                               (local-file
-                                                                                "keys/ci.guix.gnu.org.pub")
-
-                                                                               
-                                                                               (local-file
-                                                                                "keys/bordeaux.guix.gnu.org.pub"))
-                                                                       %default-authorized-guix-keys))
-                                                     (substitute-urls (append (list
-                                                                               "https://ci.guix.gnu.org.pub"
-                                                                               "https://bordeaux.guix.gnu.org.pub"))))))))))
+     (modify-services (operating-system-user-services installation-os)
+       (guix-service-type config =>
+                          (guix-configuration (inherit config)
+                                              (guix (guix-for-channels
+                                                     %my-channels))
+                                              (channels %my-channels)
+                                              (authorized-keys
+                                               %my-authorized-keys)
+                                              (substitute-urls
+                                               %my-substitute-urls)))))))
 
 my-installation-os
