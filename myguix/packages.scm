942e71e0e48820f0713f87fadf4e7de6b84f90a2
Ayan Das <bvits@riseup.net>
2024-09-08 06:18:13 +0530

    myguix: packages: Fix module.

--- myguix/packages.scm
+++ myguix/packages.scm
@@ -1,5 +1,56 @@
 (define-module (myguix packages)
-  #:use-module (gnu packages)
+  #:use-module (gnu packages admin)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages audio)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages bittorrent)
+  #:use-module (gnu packages cmake)
+  #:use-module (gnu packages commencement)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages curl)
+  #:use-module (gnu packages disk)
+  #:use-module (gnu packages emacs)
+  #:use-module (gnu packages emacs-xyz)
+  #:use-module (gnu packages file-systems)
+  #:use-module (gnu packages gimp)
+  #:use-module (gnu packages gnome)
+  #:use-module (gnu packages graphics)
+  #:use-module (gnu packages guile)
+  #:use-module (gnu packages guile-xyz)
+  #:use-module (gnu packages imagemagick)
+  #:use-module (gnu packages inkscape)
+  #:use-module (gnu packages java)
+  #:use-module (gnu packages jemalloc)
+  #:use-module (gnu packages kde)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages llvm)
+  #:use-module (gnu packages monitoring)
+  #:use-module (gnu packages music)
+  #:use-module (gnu packages networking)
+  #:use-module (gnu packages nicotine)
+  #:use-module (gnu packages node)
+  #:use-module (gnu packages password-utils)
+  #:use-module (gnu packages perl)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages rsync)
+  #:use-module (gnu packages rust)
+  #:use-module (gnu packages screen)
+  #:use-module (gnu packages search)
+  #:use-module (gnu packages shells)
+  #:use-module (gnu packages terminals)
+  #:use-module (gnu packages texlive)
+  #:use-module (gnu packages tmux)
+  #:use-module (gnu packages tree-sitter)
+  #:use-module (gnu packages version-control)
+  #:use-module (gnu packages video)
+  #:use-module (gnu packages wget)
+  #:use-module (myguix packages emacs-pqrs)
+  #:use-module (myguix packages llvm-pqrs)
+  #:use-module (myguix packages mozilla)
+  #:use-module (myguix packages python-pqrs)
+  #:use-module (myguix packages rust)
   #:export (%system-core-packages %compression-tools-packages
                                   %filesystem-management-packages
                                   %terminal-tools-packages
@@ -19,228 +70,205 @@
 
 ;; System Core Packages
 (define %system-core-packages
-  (list (specification->package "coreutils")
-        (specification->package "binutils")
-        (specification->package "findutils")
-        (specification->package "grep")
-        (specification->package "sed")
-        (specification->package "mlocate")))
+  (list coreutils
+        binutils
+        findutils
+        grep
+        sed
+        plocate))
 
 ;; Compression and archiving tools
 (define %compression-tools-packages
-  (list (specification->package "tar")
-        (specification->package "gzip")
-        (specification->package "zstd")
-        (specification->package "bzip2")
-        (specification->package "xz")
-        (specification->package "unzip")))
+  (list tar
+        gzip
+        zstd
+        bzip2
+        xz
+        unzip))
 
 ;; Filesystem and partition management tools
 (define %filesystem-management-packages
-  (list (specification->package "parted")
-        (specification->package "dosfstools")
-        (specification->package "e2fsprogs")
-        (specification->package "exfat-utils")))
+  (list parted dosfstools e2fsprogs exfat-utils))
 
 ;; Shell and terminal utilities
 (define %terminal-tools-packages
-  (list (specification->package "screen")
-        (specification->package "tmux")
-        (specification->package "alacritty")))
+  (list screen tmux alacritty))
 
 ;; Networking and file sharing tools
 (define %network-tools-packages
-  (list (specification->package "wget")
-        (specification->package "curl")
-        (specification->package "rsync")
-        (specification->package "nmap")
-        (specification->package "tcpdump")
-        (specification->package "wireshark")
-        (specification->package "traceroute")
-        (specification->package "iproute2")
-        (specification->package "net-tools")
-        (specification->package "whois")
-        (specification->package "nicotine+")
-        (specification->package "deluge")))
+  (list wget
+        curl
+        rsync
+        nmap
+        tcpdump
+        wireshark
+        traceroute
+        iproute2
+        net-tools
+        whois
+        nicotine+
+        deluge))
 
 ;; General development tools
 (define %development-tools-packages
-  (list (specification->package "git")
-        (specification->package "make")
-        (specification->package "cmake")
-        (specification->package "autoconf")
-        (specification->package "automake")
-        (specification->package "libtool")
-        (specification->package "openjdk")
-        (specification->package "pkg-config")
-        (specification->package "gcc-toolchain")
-        (specification->package "strace")
-        (specification->package "ltrace")
-        (specification->package "llvm-with-bolt")
-        (specification->package "jemalloc")
-        (specification->package "node")))
+  (list git
+        make
+        cmake
+        autoconf
+        automake
+        libtool
+        openjdk
+        pkg-config
+        gcc-toolchain
+        strace
+        ltrace
+        llvm-with-bolt
+        jemalloc
+        node))
 
 ;; Rust development tools
 (define %rust-development-packages
-  (list (specification->package "rust")
-        (specification->package "rust-analyzer")))
+  (list rust rust-analyzer))
 
 ;; Python development tools
 (define %python-development-packages
-  (list (specification->package "python")
-        (specification->package "python-jedi")
-        (specification->package "python-jedi-language-server")
-        (specification->package "python-flake8")
-        (specification->package "python-black")))
+  (list python python-jedi python-jedi-language-server python-flake8
+        python-black))
 
 ;; Guile development tools
 (define %guile-development-packages
-  (list (specification->package "guile")
-        (specification->package "guile-readline")
-        (specification->package "guile-colorized")))
+  (list guile guile-readline guile-colorized))
 
 ;; Perl development tools
 (define %perl-development-packages
-  (list (specification->package "perl")
-        (specification->package "perl-critic")
-        (specification->package "perltidy")))
+  (list perl perl-critic perltidy))
 
 ;; Emacs core packages and plugins
 (define %emacs-core-packages
-  (list (specification->package "emacs-next")
-        (specification->package "emacs-modus-themes")
-        (specification->package "emacs-doom-themes")
-        (specification->package "emacs-doom-modeline")
-        (specification->package "emacs-rainbow-delimiters")
-        (specification->package "emacs-rainbow-mode")
-        (specification->package "emacs-adaptive-wrap")
-        (specification->package "emacs-smartparens")
-        (specification->package "emacs-paredit")
-        (specification->package "emacs-nerd-icons")
-        (specification->package "emacs-nerd-icons-dired")
-        (specification->package "emacs-nerd-icons-corfu")
-        (specification->package "emacs-nerd-icons-ibuffer")
-        (specification->package "emacs-nerd-icons-completion")
-        (specification->package "emacs-kind-icon")
-        (specification->package "emacs-all-the-icons")
-        (specification->package "emacs-no-littering")
-        (specification->package "emacs-guix")
-        (specification->package "emacs-mjolnir-mode")
-        (specification->package "emacs-windsize")
-        (specification->package "emacs-ace-window")
-        (specification->package "emacs-olivetti")
-        (specification->package "emacs-which-key")
-        (specification->package "emacs-org-fragtog")
-        (specification->package "emacs-vertico")
-        (specification->package "emacs-orderless")
-        (specification->package "emacs-marginalia")
-        (specification->package "emacs-consult")
-        (specification->package "emacs-corfu")
-        (specification->package "emacs-embark")
-        (specification->package "emacs-citar")
-        (specification->package "emacs-org-roam")
-        (specification->package "emacs-org-roam-ui")
-        (specification->package "emacs-websocket")
-        (specification->package "emacs-simple-httpd")
-        (specification->package "emacs-pdf-tools")
-        (specification->package "emacs-dired-hacks")
-        (specification->package "emacs-oauth2")
-        (specification->package "emacs-pinentry")
-        (specification->package "emacs-auth-source-pass")
-        (specification->package "emacs-password-store")
-        (specification->package "emacs-yasnippet")
-        (specification->package "emacs-geiser")
-        (specification->package "emacs-geiser-guile")
-        (specification->package "emacs-lsp-mode")
-        (specification->package "emacs-lsp-ui")
-        (specification->package "emacs-flycheck")
-        (specification->package "emacs-flycheck-inline")
-        (specification->package "emacs-flycheck-guile")
-        (specification->package "emacs-flycheck-package")
-        (specification->package "emacs-flycheck-rust")
-        (specification->package "emacs-flycheck-haskell")
-        (specification->package "emacs-lsp-jedi")
-        (specification->package "emacs-lsp-haskell")
-        (specification->package "emacs-rust-mode")
-        (specification->package "emacs-rustic")
-        (specification->package "emacs-haskell-mode")
-        (specification->package "emacs-gnuplot")
-        (specification->package "emacs-lua-mode")
-        (specification->package "emacs-json-mode")
-        (specification->package "emacs-dockerfile-mode")
-        (specification->package "emacs-yaml-mode")
-        (specification->package "emacs-toml-mode")
-        (specification->package "emacs-julia-mode")
-        (specification->package "emacs-cmake-mode")
-        (specification->package "emacs-god-mode")
-        (specification->package "emacs-gptel")
-        (specification->package "emacs-magit")
-        (specification->package "emacs-auctex")
-        (specification->package "emacs-jit-spell")
-        (specification->package "emacs-web-mode")
-        (specification->package "emacs-combobulate")
-        (specification->package "emacs-markdown-mode")
-        (specification->package "emacs-vterm")
-        (specification->package "emacs-vterm-toggle")
-        (specification->package "emacs-projectile")
-        (specification->package "emacs-dashboard")
-        (specification->package "emacs-treemacs")
-        (specification->package "emacs-treemacs-extra")
-        (specification->package "emacs-lsp-treemacs")
-        (specification->package "emacs-lispy")
-        (specification->package "emacs-org-modern")
-        (specification->package "emacs-helpful")
-        (specification->package "emacs-restart-emacs")
-        (specification->package "emacs-ripgrep")
-        (specification->package "emacs-rg")
-        (specification->package "emacs-corfu-candidate-overlay")
-        (specification->package "emacs-cape")
-        (specification->package "emacs-ac-geiser")
-        (specification->package "emacs-iedit")
-        (specification->package "emacs-multiple-cursors")
-        (specification->package "emacs-arxiv-mode")
-        (specification->package "emacs-latex-snippets")))
+  (list emacs-next
+        emacs-modus-themes
+        emacs-doom-themes
+        emacs-doom-modeline
+        emacs-rainbow-delimiters
+        emacs-rainbow-mode
+        emacs-adaptive-wrap
+        emacs-smartparens
+        emacs-paredit
+        emacs-nerd-icons
+        emacs-nerd-icons-dired
+        emacs-nerd-icons-corfu
+        emacs-nerd-icons-ibuffer
+        emacs-nerd-icons-completion
+        emacs-kind-icon
+        emacs-all-the-icons
+        emacs-no-littering
+        emacs-guix
+        emacs-mjolnir-mode
+        emacs-windsize
+        emacs-ace-window
+        emacs-olivetti
+        emacs-which-key
+        emacs-org-fragtog
+        emacs-vertico
+        emacs-orderless
+        emacs-marginalia
+        emacs-consult
+        emacs-corfu
+        emacs-embark
+        emacs-citar
+        emacs-org-roam
+        emacs-org-roam-ui
+        emacs-websocket
+        emacs-simple-httpd
+        emacs-pdf-tools
+        emacs-dired-hacks
+        emacs-oauth2
+        emacs-pinentry
+        emacs-auth-source-pass
+        emacs-password-store
+        emacs-yasnippet
+        emacs-geiser
+        emacs-geiser-guile
+        emacs-lsp-mode
+        emacs-lsp-ui
+        emacs-flycheck
+        emacs-flycheck-inline
+        emacs-flycheck-guile
+        emacs-flycheck-package
+        emacs-flycheck-rust
+        emacs-flycheck-haskell
+        emacs-lsp-jedi
+        emacs-lsp-haskell
+        emacs-rust-mode
+        emacs-rustic
+        emacs-haskell-mode
+        emacs-gnuplot
+        emacs-lua-mode
+        emacs-json-mode
+        emacs-dockerfile-mode
+        emacs-yaml-mode
+        emacs-toml-mode
+        emacs-julia-mode
+        emacs-cmake-mode
+        emacs-god-mode
+        emacs-gptel
+        emacs-magit
+        emacs-auctex
+        emacs-jit-spell
+        emacs-web-mode
+        emacs-combobulate
+        emacs-markdown-mode
+        emacs-vterm
+        emacs-vterm-toggle
+        emacs-projectile
+        emacs-dashboard
+        emacs-treemacs
+        emacs-treemacs-extra
+        emacs-lsp-treemacs
+        emacs-lispy
+        emacs-org-modern
+        emacs-helpful
+        emacs-restart-emacs
+        emacs-ripgrep
+        emacs-rg
+        emacs-corfu-candidate-overlay
+        emacs-cape
+        emacs-ac-geiser
+        emacs-iedit
+        emacs-multiple-cursors
+        emacs-arxiv-mode
+        emacs-latex-snippets))
 
 ;; Tree-sitter packages for language parsing
 (define %language-support-packages
-  (list (specification->package "tree-sitter")
-        (specification->package "tree-sitter-cli")
-        (specification->package "tree-sitter-rust")
-        (specification->package "tree-sitter-python")
-        (specification->package "tree-sitter-scheme")))
+  (list tree-sitter tree-sitter-cli tree-sitter-rust tree-sitter-python
+        tree-sitter-scheme))
 
 ;; System monitoring and performance tools
 (define %system-monitoring-packages
-  (list (specification->package "htop")
-        (specification->package "sysstat")
-        (specification->package "procps")
-        (specification->package "atop")
-        (specification->package "inxi")))
+  (list htop sysstat procps atop inxi))
 
 ;; Encryption and password management tools
 (define %security-tools-packages
-  (list (specification->package "password-store")))
+  (list password-store))
 
 ;; Media and image manipulation tools
 (define %media-tools-packages
-  (list (specification->package "vlc")
-        (specification->package "yt-dlp")
-        (specification->package "imagemagick")
-        (specification->package "blender")
-        (specification->package "kdenlive")
-        (specification->package "gimp")
-        (specification->package "audacity")
-        (specification->package "inkscape")
-        (specification->package "obs")
-        (specification->package "audacious")))
+  (list vlc
+        yt-dlp
+        imagemagick
+        blender
+        kdenlive
+        gimp
+        audacity
+        inkscape
+        obs
+        audacious))
 
 ;; Desktop environment utilities
 (define %desktop-environment-packages
-  (list (specification->package "firefox")
-        (specification->package "gnome-tweaks")
-        (specification->package "gnome-boxes")
-        (specification->package "solaar")))
+  (list firefox gnome-tweaks gnome-boxes solaar))
 
 ;; TeX and document formatting tools
 (define %document-formatting-packages
-  (list (specification->package "texlive")))
+  (list texlive))
