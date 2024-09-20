375df2905b97bc1b76fc48d7685325f461ec8da9
Ayan Das <bvits@riseup.net>
2024-09-20 18:15:24 +0530

    home: emacs: Introduce my-home-emacs-service-type.
    Extends home-profile-service-type with %emacs-core-packages.

---
+++ myguix/services/home/emacs.scm
@@ -0,0 +1,17 @@
+(define-module (myguix services home emacs)
+  #:use-module (myguix packages base)
+  #:use-module (gnu home services)
+  #:use-module (gnu services)
+  #:use-module (gnu services configuration)
+  #:use-module (guix gexp)
+  #:export (my-home-emacs-service-type))
+
+(define (my-home-emacs-profile-service-type config)
+  %emacs-core-packages)
+
+(define my-home-emacs-service-type
+  (service-type (name 'home-emacs)
+                (description "Add Emacs packages to home environment profile.")
+                (extensions (list (service-extension home-profile-service-type
+                                   my-home-emacs-profile-service-type)))
+                (default-value #f)))
