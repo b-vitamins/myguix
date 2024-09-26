ea9b2509d6c5be2192b0a9a376d6de4f3d2ed08e
Ayan Das <bvits@riseup.net>
2024-09-26 21:00:14 +0530

    home: emacs: Switch to emacs-next.

--- myguix/home/services/emacs.scm
+++ myguix/home/services/emacs.scm
@@ -10,7 +10,7 @@
   #:export (my-home-emacs-service-type))
 
 (define (my-home-emacs-profile-service-type config)
-  (list emacs
+  (list emacs-next
         emacs-setup
         emacs-spinner
         emacs-modus-themes
