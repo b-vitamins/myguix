d6fcadb9807d899d740d8e36fa1f29f4509361ea
Ayan Das <bvits@riseup.net>
2024-11-02 22:06:07 +0530

    packages: bazel-6.4: Add package.

--- myguix/packages/bazel.scm
+++ myguix/packages/bazel.scm
@@ -381,3 +381,32 @@ repositories, and large numbers of users.")
                                                        "0.0.8"))
                                            (sha256 (base32
                                                     "1wx2348w49vxr3z9kjfls5zsrwr0div6r3irbvdlawan87sx5yfs"))))))))
+
+(define-public bazel-6.4
+  (package
+    (inherit bazel-6)
+    (name "bazel")
+    (version "6.4.0")
+    (source
+     (origin
+       (method url-fetch/zipbomb)
+       (uri (string-append "https://github.com/bazelbuild/bazel/"
+                           "releases/download/"
+                           version
+                           "/bazel-"
+                           version
+                           "-dist.zip"))
+       (sha256
+        (base32 "07p5yj7y864gcbwpfrcccvn52b89ml96namshbp2kfwb5ihgz25x"))
+       (patches (search-patches "patches/bazel-mock-repos.patch"
+                                "patches/bazel-workspace.patch"
+                                "patches/bazel-recreate-markers.patch"))
+       ;; This is just a start.  There are so many more jars.
+       (snippet '(for-each delete-file
+                           '("third_party/apache_commons_collections/commons-collections-3.2.2.jar"
+                             "third_party/apache_commons_compress/apache-commons-compress-1.19.jar"
+                             "third_party/apache_commons_io/commons-io-2.4.jar"
+                             "third_party/apache_commons_lang/commons-lang-2.6.jar"
+                             "third_party/hamcrest/hamcrest-core-1.3.jar"
+                             "third_party/jsr305/jsr-305.jar"
+                             "third_party/xz/xz-1.9.jar")))))))
