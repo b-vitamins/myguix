b107ecc092ce4dbe0bd29e53b81870b3617f143f
Ayan Das <bvits@riseup.net>
2024-11-03 04:03:29 +0530

    packages: cuda-toolkit: Move package (cuda -> nvidia).
    - Remove cuda-toolkit-12.1.
    - Rename cuda-toolkit-12.4 to cuda-toolkit.

--- myguix/packages/cuda.scm
+++ myguix/packages/cuda.scm
@@ -28,6 +28,7 @@
                 #:prefix license:)
   #:use-module ((myguix licenses)
                 #:prefix nonfree:)
+  #:use-module (myguix packages nvidia)
   #:use-module (gnu packages)
   #:use-module (gnu packages base)
   #:use-module (gnu packages bootstrap)
@@ -36,132 +37,6 @@
   #:use-module (gnu packages gcc)
   #:use-module (gnu packages perl)
   #:use-module (gnu packages python))
-
-(define-public cuda-toolkit-12.1
-  (package
-    (name "cuda-toolkit")
-    (version "12.1.1")
-    (source
-     (origin
-       (uri
-        "https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda_12.1.0_530.30.02_linux.run")
-       (sha256
-        (base32 "0764a8zsnmlp1f9912s8nkx700h7zzidr697mnwssw9dq4v90sb8"))
-       (method url-fetch)))
-    (supported-systems '("x86_64-linux"))
-    (build-system gnu-build-system)
-    (outputs '("out"))
-    (arguments
-     (list
-      #:modules '((guix build utils)
-                  (guix build gnu-build-system)
-                  (ice-9 match)
-                  (ice-9 ftw))
-      #:substitutable? #f
-      #:strip-binaries? #f
-      #:validate-runpath? #f
-      #:phases #~(modify-phases %standard-phases
-                   (replace 'unpack
-                     (lambda* (#:key inputs #:allow-other-keys)
-                       (define ld.so
-                         (search-input-file inputs
-                                            #$(glibc-dynamic-linker)))
-                       (let ((source (assoc-ref inputs "source")))
-                         (invoke "sh" source "--keep" "--noexec")
-                         (chdir "pkg"))))
-                   (delete 'configure)
-                   (delete 'check)
-                   (replace 'build
-                     (lambda* (#:key inputs #:allow-other-keys)
-                       (define libc
-                         (assoc-ref inputs "libc"))
-                       (define gcc-lib
-                         (assoc-ref inputs "gcc:lib"))
-                       (define ld.so
-                         (search-input-file inputs
-                                            #$(glibc-dynamic-linker)))
-                       (define rpath
-                         (string-join (list "$ORIGIN"
-                                            (string-append #$output "/lib")
-                                            (string-append #$output
-                                                           "/nvvm/lib64")
-                                            (string-append libc "/lib")
-                                            (string-append gcc-lib "/lib"))
-                                      ":"))
-                       (define (patch-elf file)
-                         (make-file-writable file)
-                         (format #t "Setting RPATH on '~a'...~%" file)
-                         ;; RPATH should be modified before the interpreter. If
-                         ;; done the other way around, it nukes the resulting
-                         ;; binary.
-                         (invoke "patchelf" "--set-rpath" rpath
-                                 "--force-rpath" file)
-                         (unless (string-contains file ".so")
-                           (format #t "Setting interpreter on '~a'...~%" file)
-                           (invoke "patchelf" "--set-interpreter" ld.so file)))
-                       (for-each (lambda (file)
-                                   (when (elf-file? file)
-                                     (patch-elf file)))
-                                 (find-files "."
-                                             (lambda (file stat)
-                                               (eq? 'regular
-                                                    (stat:type stat)))))))
-                   (replace 'install
-                     (lambda _
-                       (define (copy-from-directory directory)
-                         (for-each (lambda (entry)
-                                     (define sub-directory
-                                       (string-append directory "/" entry))
-
-                                     (define target
-                                       (string-append #$output "/"
-                                                      (basename entry)))
-
-                                     (when (file-exists? sub-directory)
-                                       (copy-recursively sub-directory target)))
-                                   '("bin" "targets/x86_64-linux/lib"
-                                     "targets/x86_64-linux/include" "nvvm/bin"
-                                     "nvvm/include" "nvvm/lib64")))
-
-                       (setenv "COLUMNS" "200")
-                       (with-directory-excursion "builds"
-                         (for-each copy-from-directory
-                                   (scandir "."
-                                            (match-lambda
-                                              ((or "." "..")
-                                               #f)
-                                              (_ #t))))
-                         (copy-recursively "cuda_nvcc/nvvm/libdevice"
-                                           (string-append #$output
-                                                          "/nvvm/libdevice")))))
-                   (add-after 'install 'install-cupti
-                     (lambda _
-                       (copy-recursively "builds/cuda_cupti/extras/CUPTI"
-                                         #$output)))
-                   (add-after 'install 'delete-stray-symlinks
-                     (lambda _
-                       (delete-file (string-append #$output "/include/include")))))))
-    (native-inputs (list which patchelf perl python-2))
-    (inputs `(("gcc:lib" ,gcc-11 "lib")))
-    (home-page "https://developer.nvidia.com/cuda-toolkit")
-    (synopsis "Compiler for the CUDA language and associated run-time support")
-    (description
-     "This package provides the CUDA compiler and the CUDA run-time support
-libraries for NVIDIA GPUs, all of which are proprietary.")
-    (license (nonfree:nonfree
-              "https://developer.nvidia.com/nvidia-cuda-license"))))
-
-(define-public cuda-toolkit-12.4
-  (package
-    (inherit cuda-toolkit-12.1)
-    (version "12.1.4")
-    (source
-     (origin
-       (uri
-        "https://developer.download.nvidia.com/compute/cuda/12.4.0/local_installers/cuda_12.4.0_550.54.14_linux.run")
-       (sha256
-        (base32 "05vxwn91hhrc57p8vr3xi5dbjiwdnwdnp2xnrmshajd9xks45a76"))
-       (method url-fetch)))))
 
 (define-public cudnn
   (package
