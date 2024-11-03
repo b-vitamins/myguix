c2702c4307cc52b6f5be52ca3974260f9d46d463
Ayan Das <bvits@riseup.net>
2024-11-03 11:11:34 +0530

    packages: base: Refactor for modularity.
    - Split `%filesystem-management-packages' into `%basic-filesystem-tools', `%ssd-tools', `%advanced-filesystem-tools', and `%diagnostic-and-maintenance-tools'.
    - Split and expanded `%network-tools-packages' to `%network-analysis-tools-packages' and `%network-utilities-packages'.
    - Split `%development-tools-packages' into new categories: `%build-system-packages', `%debugging-tools-packages', `%memory-and-optimization-tools-packages'.
    - `%media-tools-packages' has been divided into `%media-consumption-packages', `%audio-conversion-tools-packages', `%video-conversion-tools-packages', `%graphic-production-packages', `%audio-production-packages', and `%video-production-packages'.
    - Introduced `%nvidia-gpu-packages' to specifically handle NVIDIA GPU related utilities.
    - Split and extended document related tools into `%document-conversion-tools-packages', `%document-authoring-packages', and `%document-manipulation-packages'.
    - `%desktop-environment-packages' expanded and clarified as `%desktop-utilities-packages'.
    - Added `%opencog-packages' for specific OpenCog development tools.
    - Consolidated various font packages into `%general-purpose-fonts', `%document-fonts', `%fira-fonts', `%adobe-fonts', `%google-fonts', `%iosevka-fonts', `%monospace-fonts', `%sans-serif-fonts', `%serif-fonts', `%cjk-fonts', and `%unicode-fonts'.

--- myguix/packages/base.scm
+++ myguix/packages/base.scm
@@ -1,5 +1,6 @@
 (define-module (myguix packages base)
   #:use-module (gnu packages admin)
+  #:use-module (gnu packages antivirus)
   #:use-module (gnu packages autotools)
   #:use-module (gnu packages audio)
   #:use-module (gnu packages backup)
@@ -12,14 +13,26 @@
   #:use-module (gnu packages compression)
   #:use-module (gnu packages curl)
   #:use-module (gnu packages disk)
+  #:use-module (gnu packages djvu)
+  #:use-module (gnu packages ebook)
+  #:use-module (gnu packages elf)
   #:use-module (gnu packages emacs)
   #:use-module (gnu packages emacs-xyz)
+  #:use-module (gnu packages enlightenment)
   #:use-module (gnu packages file-systems)
+  #:use-module (gnu packages fonts)
+  #:use-module (gnu packages fontutils)
+  #:use-module (gnu packages ftp)
+  #:use-module (gnu packages gdb)
+  #:use-module (gnu packages ghostscript)
   #:use-module (gnu packages gimp)
   #:use-module (gnu packages gnome)
+  #:use-module (gnu packages gnupg)
+  #:use-module (gnu packages gnuzilla)
   #:use-module (gnu packages graphics)
   #:use-module (gnu packages guile)
   #:use-module (gnu packages guile-xyz)
+  #:use-module (gnu packages gpodder)
   #:use-module (gnu packages haskell-xyz)
   #:use-module (gnu packages hunspell)
   #:use-module (gnu packages imagemagick)
@@ -33,10 +46,13 @@
   #:use-module (gnu packages mp3)
   #:use-module (gnu packages music)
   #:use-module (gnu packages networking)
+  #:use-module (gnu packages nfs)
   #:use-module (gnu packages nicotine)
   #:use-module (gnu packages node)
+  #:use-module (gnu packages opencog)
   #:use-module (gnu packages parallel)
   #:use-module (gnu packages password-utils)
+  #:use-module (gnu packages pciutils)
   #:use-module (gnu packages pdf)
   #:use-module (gnu packages perl)
   #:use-module (gnu packages photo)
@@ -47,15 +63,20 @@
   #:use-module (gnu packages rsync)
   #:use-module ((gnu packages rust)
                 #:hide (rust))
+  #:use-module (gnu packages samba)
   #:use-module (gnu packages screen)
+  #:use-module (gnu packages scribus)
+  #:use-module (gnu packages scsi)
   #:use-module (gnu packages search)
   #:use-module (gnu packages shells)
+  #:use-module (gnu packages storage)
   #:use-module (gnu packages sync)
   #:use-module (gnu packages terminals)
   #:use-module (gnu packages texlive)
   #:use-module (gnu packages tls)
   #:use-module (gnu packages tmux)
   #:use-module (gnu packages tree-sitter)
+  #:use-module (gnu packages upnp)
   #:use-module (gnu packages version-control)
   #:use-module (gnu packages video)
   #:use-module (gnu packages wget)
@@ -68,278 +89,344 @@
   #:use-module (myguix packages python-pqrs)
   #:use-module (myguix packages rust)
   #:use-module (myguix packages video)
-  #:export (%system-core-packages %compression-tools-packages
-                                  %filesystem-management-packages
+  #:export (%system-core-packages %nvidia-gpu-packages
                                   %terminal-tools-packages
-                                  %network-tools-packages
-                                  %development-tools-packages
+                                  %secret-mgmt-packages
+                                  %desktop-utilities-packages
+                                  %system-monitoring-packages
+                                  %basic-filesystem-tools
+                                  %basic-filesystem-tools
+                                  %ssd-tools
+                                  %diagnostic-and-maintenance-tools
+                                  %remote-storage-mount-packages
+                                  %advanced-filesystem-tools
+                                  %compression-tools-packages
+                                  %media-consumption-packages
+                                  %audio-conversion-tools-packages
+                                  %video-conversion-tools-packages
+                                  %document-conversion-tools-packages
+                                  %graphic-production-packages
+                                  %audio-production-packages
+                                  %video-production-packages
+                                  %document-authoring-packages
+                                  %document-manipulation-packages
+                                  %file-transfer-tools-packages
+                                  %network-analysis-tools-packages
+                                  %p2p-file-sharing-packages
+                                  %network-utilities-packages
+                                  %general-purpose-fonts
+                                  %document-fonts
+                                  %fira-fonts
+                                  %adobe-fonts
+                                  %google-fonts
+                                  %iosevka-fonts
+                                  %monospace-fonts
+                                  %sans-serif-fonts
+                                  %serif-fonts
+                                  %cjk-fonts
+                                  %unicode-fonts
+                                  %version-control-packages
+                                  %build-system-packages
+                                  %debugging-tools-packages
+                                  %memory-and-optimization-tools-packages
+                                  %runtime-packages
+                                  %tree-sitter-packages
+                                  %guile-development-packages
                                   %rust-development-packages
                                   %python-development-packages
-                                  %guile-development-packages
                                   %perl-development-packages
-                                  %emacs-core-packages
-                                  %language-support-packages
-                                  %system-monitoring-packages
-                                  %security-tools-packages
-                                  %media-tools-packages
-                                  %nvidia-gpu-packages
-                                  %desktop-environment-packages
-                                  %document-formatting-packages))
+                                  %opencog-packages))
 
-;; System Core Packages
 (define %system-core-packages
-  (list coreutils
+  (list clamav
+        coreutils
         binutils
         findutils
+        pciutils
         tree
         grep
         sed
-        plocate))
+        plocate
+        fontconfig))
 
-;; Compression and archiving tools
-(define %compression-tools-packages
-  (list tar
-        gzip
-        zstd
-        bzip2
-        xz
-        unzip))
-
-;; Filesystem and partition management tools
-(define %filesystem-management-packages
-  (list parted dosfstools e2fsprogs exfat-utils))
-
-;; Shell and terminal utilities
-(define %terminal-tools-packages
-  (list parallel screen tmux alacritty))
-
-;; Networking and file sharing tools
-(define %network-tools-packages
-  (list wget
-        curl
-        rsync
-        rclone
-        borg
-        borgmatic
-        nmap
-        tcpdump
-        wireshark
-        traceroute
-        net-tools
-        whois
-        nicotine+
-        deluge
-        qbittorrent-enhanced
-        transmission
-        aria2
-        megacmd
-        openssl
-        speedtest-cli
-        libnatpmp
-        python-nodriver))
-
-;; General development tools
-(define %development-tools-packages
-  (list git
-        git-lfs
-        git-tools
-        git-remote-gcrypt
-        git-crypt
-        pass-git-helper
-        cmake
-        autoconf
-        automake
-        libtool
-        openjdk
-        pkg-config
-        gcc-toolchain
-        strace
-        ltrace
-        llvm-with-bolt-17
-        jemalloc
-        node))
-
-;; Rust development tools
-(define %rust-development-packages
-  (list rust rust-analyzer))
-
-;; Python development tools
-(define %python-development-packages
-  (list python python-jedi python-jedi-language-server python-flake8
-        python-black))
-
-;; Guile development tools
-(define %guile-development-packages
-  (list guile-3.0 guile-readline guile-colorized))
-
-;; Perl development tools
-(define %perl-development-packages
-  (list perl perl-critic perltidy))
-
-;; Emacs core packages and plugins
-(define %emacs-core-packages
-  (list emacs-setup
-        emacs-spinner
-        emacs-modus-themes
-        emacs-doom-themes
-        emacs-doom-modeline
-        emacs-rainbow-delimiters
-        emacs-rainbow-mode
-        emacs-adaptive-wrap
-        emacs-smartparens
-        emacs-paredit
-        emacs-nerd-icons
-        emacs-nerd-icons-dired
-        emacs-nerd-icons-corfu
-        emacs-nerd-icons-ibuffer
-        emacs-nerd-icons-completion
-        emacs-kind-icon
-        emacs-all-the-icons
-        emacs-no-littering
-        emacs-exec-path-from-shell
-        emacs-guix
-        emacs-mjolnir-mode
-        emacs-windsize
-        emacs-ace-window
-        emacs-olivetti
-        emacs-which-key
-        emacs-org-fragtog
-        emacs-vertico
-        emacs-orderless
-        emacs-marginalia
-        emacs-compat
-        emacs-consult
-        emacs-consult-org-roam
-        emacs-corfu
-        emacs-embark
-        emacs-citar
-        emacs-org-roam
-        emacs-org-roam-ui
-        emacs-websocket
-        emacs-simple-httpd
-        emacs-pdf-tools
-        emacs-dired-hacks
-        emacs-oauth2
-        emacs-pinentry
-        emacs-auth-source-pass
-        emacs-password-store
-        emacs-yasnippet
-        emacs-geiser
-        emacs-geiser-guile
-        emacs-lsp-mode
-        emacs-lsp-ui
-        emacs-flycheck
-        emacs-flycheck-inline
-        emacs-flycheck-guile
-        emacs-flycheck-package
-        emacs-flycheck-rust
-        emacs-flycheck-haskell
-        emacs-lsp-jedi
-        emacs-lsp-haskell
-        emacs-rust-mode
-        emacs-rustic
-        emacs-ht
-        emacs-hydra
-        emacs-let-alist
-        emacs-haskell-mode
-        emacs-gnuplot
-        emacs-lua-mode
-        emacs-json-mode
-        emacs-dockerfile-mode
-        emacs-yaml-mode
-        emacs-toml-mode
-        emacs-julia-mode
-        emacs-cmake-mode
-        emacs-god-mode
-        emacs-gptel
-        emacs-magit
-        emacs-auctex
-        emacs-jit-spell
-        emacs-web-mode
-        emacs-combobulate
-        emacs-markdown-mode
-        emacs-vterm
-        emacs-vterm-toggle
-        emacs-projectile
-        emacs-dash
-        emacs-eldoc
-        emacs-dashboard
-        emacs-f
-        emacs-tablist
-        emacs-treemacs
-        emacs-treemacs-extra
-        emacs-lsp-treemacs
-        emacs-lispy
-        emacs-org-modern
-        emacs-helpful
-        emacs-restart-emacs
-        emacs-ripgrep
-        emacs-rg
-        emacs-corfu-candidate-overlay
-        emacs-cape
-        emacs-ac-geiser
-        emacs-iedit
-        emacs-s
-        emacs-seq
-        emacs-multiple-cursors
-        emacs-arxiv-mode))
-
-;; Tree-sitter packages for language parsing
-(define %language-support-packages
-  (list tree-sitter tree-sitter-cli tree-sitter-rust tree-sitter-python
-        tree-sitter-scheme))
-
-;; System monitoring and performance tools
-(define %system-monitoring-packages
-  (list htop sysstat procps atop inxi))
-
-;; NVIDIA GPU related packages
 (define %nvidia-gpu-packages
   (list cuda-toolkit
         nvidia-htop
         nvidia-system-monitor
         nvidia-settings
         gpustat
-        ffmpeg-cuda))
+        ffmpeg-cuda
+        mpv-cuda))
 
-;; Encryption and password management tools
-(define %security-tools-packages
-  (list password-store))
+(define %terminal-tools-packages
+  (list parallel screen tmux alacritty))
 
-;; Media and image manipulation tools
-(define %media-tools-packages
+(define %secret-mgmt-packages
+  (list gnupg pinentry password-store))
+
+(define %desktop-utilities-packages
+  (list ungoogled-chromium/wayland
+        firefox
+        icedove
+        gpodder
+        solaar
+        gnome-tweaks
+        gnome-boxes))
+
+(define %system-monitoring-packages
+  (list htop sysstat procps atop inxi))
+
+(define %basic-filesystem-tools
+  (list parted fdisk dosfstools e2fsprogs exfat-utils))
+
+(define %ssd-tools
+  (list nvme-cli bcache-tools lsscsi))
+
+(define %diagnostic-and-maintenance-tools
+  (list smartmontools hdparm blktrace))
+
+(define %remote-storage-mount-packages
+  (list filezilla
+        sshfs
+        davfs2
+        cifs-utils
+        samba
+        wsdd
+        ncftp
+        vsftpd
+        lftp))
+
+(define %advanced-filesystem-tools
+  (list xfsprogs
+        btrfs-progs
+        jfsutils
+        f2fs-tools
+        ntfs-3g
+        glusterfs
+        ceph
+        lvm2
+        nfs-utils
+        mdadm))
+
+(define %compression-tools-packages
+  (list tar
+        gzip
+        zstd
+        bzip2
+        xz
+        unzip
+        p7zip
+        unrar-free
+        lz4
+        lrzip))
+
+(define %media-consumption-packages
+  (list vlc audacious cozy ephoto))
+
+(define %audio-conversion-tools-packages
   (list abcde
-        mpv-cuda
-        celluloid
-        vlc
-        qpdf
-        imagemagick
-        mkvtoolnix
-        handbrake
         sox
         lame
         flac
-        yt-dlp
-        blender
-        kdenlive
-        gimp
-        audacity
-        inkscape
-        obs
-        audacious
-        pandoc
+        easytag
+        opus
+        opus-tools
+        opustags
+        soundconverter
+        r128gain))
+
+(define %video-conversion-tools-packages
+  (list handbrake yt-dlp brasero asunder mkvtoolnix))
+
+(define %document-conversion-tools-packages
+  (list pandoc
+        img2pdf
+        pdfgrep
+        pdf2svg
+        pdf2djvu
+        djvu2pdf
+        weasyprint))
+
+(define %graphic-production-packages
+  (list gimp inkscape imagemagick graphicsmagick))
+
+(define %audio-production-packages
+  (list ardour audacity ecasound soundtouch))
+
+(define %video-production-packages
+  (list blender kdenlive obs))
+
+(define %document-authoring-packages
+  (list texlive scribus hunspell hunspell-dict-en-us hunspell-dict-en-gb-ize))
+
+(define %document-manipulation-packages
+  (list qpdf
+        mupdf
+        pdfarranger
+        stapler
+        diffpdf
         mediainfo
-        perl-image-exiftool))
+        perl-image-exiftool
+        psutils
+        ghostscript
+        pdfposter
+        poppler))
 
-;; Desktop environment utilities
-(define %desktop-environment-packages
-  (list ungoogled-chromium/wayland
-        firefox
-        gnome-tweaks
-        gnome-boxes
-        solaar
-        xvfb-run))
+(define %file-transfer-tools-packages
+  (list wget
+        curl
+        megacmd
+        s3cmd
+        rsync
+        rclone
+        borg
+        borgmatic
+        axel))
 
-;; TeX and document formatting tools
-(define %document-formatting-packages
-  (list texlive hunspell hunspell-dict-en-us hunspell-dict-en-gb-ize))
+(define %network-analysis-tools-packages
+  (list nmap tcpdump wireshark traceroute speedtest-cli))
+
+(define %p2p-file-sharing-packages
+  (list deluge
+        qbittorrent-enhanced
+        transmission
+        aria2
+        nicotine+
+        miniupnpc))
+
+(define %network-utilities-packages
+  (list net-tools whois openssl libnatpmp python-nodriver))
+
+(define %general-purpose-fonts
+  (list font-dejavu font-bitstream-vera font-abattis-cantarell font-liberation
+        font-carlito))
+
+(define %document-fonts
+  (list font-libre-franklin
+        font-latin-modern
+        font-charter
+        font-tex-gyre
+        font-cardo
+        font-gfs-ambrosia
+        font-cormorant
+        font-amiri
+        font-sil-gentium
+        font-sil-charis))
+
+(define %fira-fonts
+  (list font-fira-sans font-fira-mono font-fira-go font-fira-code))
+
+(define %adobe-fonts
+  (list font-adobe-source-han-sans font-adobe-source-code-pro
+        font-adobe-source-sans-pro font-adobe-source-serif-pro))
+
+(define %google-fonts
+  (list font-google-noto
+        font-google-noto-emoji
+        font-google-noto-sans-cjk
+        font-google-noto-serif-cjk
+        font-google-roboto
+        font-google-material-design-icons))
+
+(define %iosevka-fonts
+  (list font-iosevka
+        font-iosevka-slab
+        font-iosevka-term
+        font-iosevka-term-slab
+        font-iosevka-aile
+        font-iosevka-curly
+        font-iosevka-curly-slab
+        font-iosevka-etoile
+        font-iosevka-ss08
+        font-iosevka-ss09
+        font-iosevka-comfy))
+
+(define %monospace-fonts
+  (list font-inconsolata
+        font-intel-one-mono
+        font-borg-sans-mono
+        font-jetbrains-mono
+        font-juliamono
+        font-victor-mono
+        font-hack
+        font-terminus
+        font-anonymous-pro
+        font-plemoljp
+        font-hermit))
+
+(define %sans-serif-fonts
+  (list font-lato
+        font-public-sans
+        font-dosis
+        font-space-grotesk
+        font-oswald
+        font-catamaran
+        font-overpass))
+
+(define %serif-fonts
+  (list font-linuxlibertine
+        font-libre-franklin
+        font-latin-modern
+        font-charter
+        font-cardo
+        font-gfs-ambrosia
+        font-cormorant
+        font-amiri))
+
+(define %cjk-fonts
+  (list font-wqy-zenhei
+        font-wqy-microhei
+        font-cns11643
+        font-cns11643-swjz
+        font-sarasa-gothic
+        font-lxgw-wenkai
+        font-lxgw-wenkai-tc))
+
+(define %unicode-fonts
+  (list font-gnu-freefont font-gnu-unifont font-ipa font-ipa-ex))
+
+(define %version-control-packages
+  (list git
+        git-lfs
+        git-tools
+        git-remote-gcrypt
+        git-crypt
+        pass-git-helper))
+
+(define %build-system-packages
+  (list cmake
+        autoconf
+        automake
+        libtool
+        openjdk
+        pkg-config
+        gcc-toolchain))
+
+(define %debugging-tools-packages
+  (list gdb patchelf strace ltrace))
+
+(define %memory-and-optimization-tools-packages
+  (list jemalloc llvm-with-bolt))
+
+(define %runtime-packages
+  (list node openjdk))
+
+(define %tree-sitter-packages
+  (list tree-sitter tree-sitter-cli tree-sitter-rust tree-sitter-python
+        tree-sitter-scheme))
+
+(define %guile-development-packages
+  (list guile-3.0 guile-readline guile-colorized))
+
+(define %rust-development-packages
+  (list rust rust-analyzer))
+
+(define %python-development-packages
+  (list python python-jedi python-jedi-language-server python-flake8
+        python-black))
+
+(define %perl-development-packages
+  (list perl perl-critic perltidy))
+
+(define %opencog-packages
+  (list opencog cogutil cogserver attention atomspace))
