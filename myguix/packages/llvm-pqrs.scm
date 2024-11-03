736219e50f56f4550cff960a982869c2beb362ac
Ayan Das <bvits@riseup.net>
2024-11-03 11:02:01 +0530

    packages: llvm-with-bolt-18: Add package.

--- myguix/packages/llvm-pqrs.scm
+++ myguix/packages/llvm-pqrs.scm
@@ -5,7 +5,7 @@
   #:use-module (guix utils))
 
 (define-public llvm-with-bolt
-  llvm-with-bolt-17)
+  llvm-with-bolt-18)
 
 (define-public llvm-with-bolt-17
   (package
@@ -19,3 +19,14 @@
                   (list "-DLLVM_ENABLE_ASSERTIONS=ON"
                         "-DLLVM_ENABLE_PROJECTS=bolt")))))))
 
+(define-public llvm-with-bolt-18
+  (package
+    (inherit llvm-18)
+    (name "llvm-with-bolt")
+    (arguments
+     (substitute-keyword-arguments (package-arguments llvm-18)
+       ((#:configure-flags flags)
+        #~(append #$flags
+                  ;; Add BOLT support
+                  (list "-DLLVM_ENABLE_ASSERTIONS=ON"
+                        "-DLLVM_ENABLE_PROJECTS=bolt")))))))
