897e1182bc0148543cdce538d9388ff229910d9f
Ayan Das <bvits@riseup.net>
2024-11-25 00:40:03 +0530

    home: home-zsh-service-type: Simplify .zshrc.

--- myguix/home.scm
+++ myguix/home.scm
@@ -30,26 +30,7 @@ XTerm*metaSendsEscape: false
                                          (zshenv (list (local-file
                                                         "etc/zsh/zshenv")))
                                          (zshrc (list (local-file
-                                                       "etc/zsh/zshrc")
-                                                      (mixed-text-file "zshrc"
-                                                       "source "
-                                                       zsh-syntax-highlighting
-                                                       "/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh")
-                                                      (mixed-text-file "zshrc"
-                                                       "source "
-                                                       zsh-history-substring-search
-                                                       "/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh")
-                                                      (mixed-text-file "zshrc"
-                                                       "fpath+=\"${0:A:h}"
-                                                       zsh-completions
-                                                       "share/zsh/site-functions/\"")
-                                                      (mixed-text-file "zshrc"
-                                                       "source "
-                                                       zsh-autosuggestions
-                                                       "/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh")
-                                                      (mixed-text-file "zshrc"
-                                                       "source " zsh-autopair
-                                                       "/share/zsh/plugins/zsh-autopair/zsh-autopair.zsh")))))
+                                                       "etc/zsh/zshrc")))))
 
         (service home-inputrc-service-type
                  (home-inputrc-configuration (key-bindings `(("Control-l" . "clear-screen")
