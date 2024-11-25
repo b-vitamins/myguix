fd95e230646e3d38e96512d0e3ce1e91d4c94ae1
Ayan Das <bvits@riseup.net>
2024-11-25 08:55:48 +0530

    home: home-zsh-service-type: Revert to using plain-files.

--- myguix/home.scm
+++ myguix/home.scm
@@ -26,11 +26,107 @@ XTerm*metaSendsEscape: false
                    ("nano/nanorc" ,%default-nanorc)))
 
         (service home-zsh-service-type
-                 (home-zsh-configuration (xdg-flavor? #t)
-                                         (zshenv (list (local-file
-                                                        "etc/zsh/zshenv")))
-                                         (zshrc (list (local-file
-                                                       "etc/zsh/zshrc")))))
+                 (home-zsh-configuration (environment-variables '(("PROMPT" . "$%F{39}%n@%m%f %F{34}%~%f %F{160}>%f ")
+                                                                  ("GUILE_AUTO_COMPILE" . "0")
+                                                                  ("HISTFILE" . "$XDG_CONFIG_HOME/zsh/.zhistory")
+                                                                  ("HISTSIZE" . "10000")
+                                                                  ("SAVEHIST" . "10000")))
+                                         (zshrc `(,(plain-file "zprofile"
+                                                               (string-append
+                                                                "if [ -f \"$HOME/.profile\" ]; then\n"
+                                                                "    source \"$HOME/.profile\"\n"
+                                                                "fi\n")) ,(plain-file
+                                                                           "add-zsh-hook"
+                                                                           "autoload -Uz add-zsh-hook\n")
+                                                  ,(plain-file "compinit"
+                                                               (string-append
+                                                                "autoload -Uz compinit\n"
+                                                                "compinit -u\n"))
+                                                  ,(plain-file "colors"
+                                                               (string-append
+                                                                "autoload -Uz colors\n"
+                                                                "colors\n"))
+                                                  ,(plain-file "setopt"
+                                                               (string-append
+                                                                "setopt NO_BEEP\n"
+                                                                "setopt EXTENDED_GLOB\n"
+                                                                "setopt NO_CLOBBER\n"
+                                                                "setopt NO_SHARE_HISTORY\n"
+                                                                "setopt APPEND_HISTORY\n"
+                                                                "setopt INC_APPEND_HISTORY\n"
+                                                                "setopt HIST_IGNORE_DUPS\n"
+                                                                "setopt HIST_IGNORE_SPACE\n"
+                                                                "setopt HIST_REDUCE_BLANKS\n"))
+                                                  ,(plain-file
+                                                    "completion-options"
+                                                    (string-append
+                                                     "zstyle ':completion:*' menu select\n"
+                                                     "zstyle ':completion:*' list-suffixes 'yes'
+"
+                                                     "zstyle ':completion:*' expand 'yes'\n"
+                                                     "zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+"
+                                                     "zstyle ':completion:*' use-cache on\n"
+                                                     "zstyle ':completion:*' cache-path \"$XDG_CONFIG_HOME/zsh/.zcompdump\"
+"))
+                                                  ,(plain-file "colors"
+                                                               "colors\n")
+                                                  ,(plain-file "functions"
+                                                               (string-append
+                                                                "function is_utf8_locale {\n"
+                                                                "    [[ \"${LC_ALL:-}${LANG:-}${GDM_LANG:-}\" =~ [Uu][Tt][Ff]-?8 ]]
+"
+                                                                "}\n\n"
+                                                                "function prompt_env_precmd {\n"
+                                                                "    local display_time\n"
+                                                                "    [[ \"$SHOW_TIMESTAMP\" -eq 1 ]] && display_time=\"%F{#ECBE7B}%D{%H:%M:%S}%f %F{#88c0d0}%D{%d %b}%f \"
+
+"
+                                                                "    if is_utf8_locale; then\n"
+                                                                "        if [[ -n \"$GUIX_ENVIRONMENT\" ]]; then
+"
+                                                                "            export PROMPT=\"${display_time}%F{#51afef}環境%f %F{#98be65}%2~%f %F{#ff6c6b}>%f \"
+"
+                                                                "        else\n"
+                                                                "            export PROMPT=\"${display_time}%F{#ff6c6b}カオス%f %F{#98be65}%2~%f %F{#51afef}>%f \"
+"
+                                                                "        fi\n"
+                                                                "    else\n"
+                                                                "        if [[ -n \"$GUIX_ENVIRONMENT\" ]]; then
+"
+                                                                "            export PROMPT=\"${display_time}%F{39}%n@%m%f %F{34}%~%f %F{220}[env]%f %F{160}>%f \"
+"
+                                                                "        else\n"
+                                                                "            export PROMPT=\"${display_time}%F{39}%n@%m%f %F{34}%~%f %F{160}>%f \"
+"
+                                                                "        fi\n"
+                                                                "    fi\n"
+                                                                "}\n\n"
+                                                                "function toggle_timestamp {\n"
+                                                                "    SHOW_TIMESTAMP=$((1 - SHOW_TIMESTAMP))
+"
+                                                                "    prompt_env_precmd\n"
+                                                                "    zle reset-prompt\n"
+                                                                "}\n\n"
+                                                                "zle -N toggle_timestamp\n"
+                                                                "bindkey '^@' toggle_timestamp\n\n"
+                                                                "add-zsh-hook precmd prompt_env_precmd\n"))
+                                                  ,(plain-file "keybindings"
+                                                               (string-append
+                                                                "bindkey '^[[A' history-search-backward\n"
+                                                                "bindkey '^[[B' history-search-forward\n"))
+                                                  ,(plain-file "trap-alarm"
+                                                    "TRAPALRM() { zle reset-prompt }\nTMOUT=1\n")
+                                                  ,(plain-file "aliases"
+                                                               (string-append
+                                                                "alias ls='ls --color=auto'\n"
+                                                                "alias grep='grep --color=auto'\n"
+                                                                "alias diff='diff --color=auto'\n"
+                                                                "alias ll='ls -lh --color=auto'\n"
+                                                                "alias la='ls -A --color=auto'\n"
+                                                                "alias e='emacs'\n"
+                                                                "alias ec='emacsclient -c'\n"
+                                                                "alias em='emacs -nw'\n"))))))
 
         (service home-inputrc-service-type
                  (home-inputrc-configuration (key-bindings `(("Control-l" . "clear-screen")
