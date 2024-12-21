c6844fe2ec89f572e9f333c5d43334aa0ae8009e
Ayan Das <bvits@riseup.net>
2024-12-21 16:46:20 +0530

    ci: Add module.

---
+++ myguix/ci.scm
@@ -0,0 +1,20 @@
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+;;; Copyright © 2021 Mathieu Othacehe <m.othacehe@gmail.com>
+
+(define-module (myguix ci)
+  #:use-module (gnu ci)
+  #:use-module (gnu system image)
+  #:use-module (myguix system install)
+  #:use-module (srfi srfi-1)
+  #:export (cuirass-jobs))
+
+(define (cuirass-jobs store arguments)
+  (define systems
+    (arguments->systems arguments))
+
+  (append-map (lambda (system)
+                (list (image->job store
+                                  (image-with-os iso9660-image
+                                                 installation-os-nonfree)
+                                  #:name "nonfree-iso9660-image"
+                                  #:system system))) systems))
