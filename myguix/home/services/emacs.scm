9a0acd50f00aecc316385629292a4bb65b25acdc
Ayan Das <bvits@riseup.net>
2025-01-31 23:23:49 +0530

    home: emacs: Revert from emacs-next to emacs.

--- myguix/home/services/emacs.scm
+++ myguix/home/services/emacs.scm
@@ -10,7 +10,7 @@
   #:export (my-home-emacs-service-type))
 
 (define (my-home-emacs-profile-service-type config)
-  (list emacs-next
+  (list emacs
         emacs-ac-geiser
         emacs-academic-phrases
         emacs-ace-window
