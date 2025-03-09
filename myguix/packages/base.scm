3433207fb9927bfb63cae854379975e4682830d5
Ayan Das <bvits@riseup.net>
2025-03-09 09:49:24 +0530

    packages: %deep-learning-framework-packages: Remove python-tensorflow-cuda.

--- myguix/packages/base.scm
+++ myguix/packages/base.scm
@@ -478,8 +478,7 @@
         python-scikit-learn-extra
         python-pytorch-cuda
         python-torchvision-cuda
-        python-torchdata-cuda
-        python-tensorflow-cuda))
+        python-torchdata-cuda))
 
 (define %huggingface-packages
   (list python-huggingface-hub
