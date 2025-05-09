4c93dc42ba94e084a8dc42ab7ae2c983049782f5
Ayan Das <bvits@riseup.net>
2025-05-09 23:04:35 +0530

    home: emacs: Trim the fat.

--- myguix/home/services/emacs.scm
+++ myguix/home/services/emacs.scm
@@ -10,56 +10,27 @@
   #:export (my-home-emacs-service-type))
 
 (define (my-home-emacs-profile-service-type config)
-  (list emacs
-        emacs-ac-geiser
-        emacs-academic-phrases
+  (list emacs-next
         emacs-ace-window
         emacs-adaptive-wrap
-        emacs-ag
-        emacs-aio
         emacs-all-the-icons
-        emacs-arei
         emacs-arxiv-mode
-        emacs-async-await
         emacs-auctex
         emacs-auth-source-pass
-        emacs-auto-complete
-        emacs-blacken
         emacs-cape
-        emacs-cargo-el
         emacs-citar
-        emacs-citar-org-roam
         emacs-cmake-mode
-        emacs-combobulate
-        emacs-compat
         emacs-consult
-        emacs-consult-bibtex
-        emacs-consult-dir
-        emacs-consult-flycheck
         emacs-consult-lsp
         emacs-consult-org-roam
-        emacs-consult-yasnippet
         emacs-corfu
-        emacs-corfu-candidate-overlay
-        emacs-dash
-        emacs-dashboard
-        emacs-deadgrep
-        emacs-devil
         emacs-dired-hacks
+        emacs-direnv
         emacs-dockerfile-mode
         emacs-doom-modeline
         emacs-doom-themes
-        emacs-eldoc
-        emacs-elegant-agenda-mode
-        emacs-elfeed
-        emacs-elfeed-goodies
-        emacs-elfeed-org
-        emacs-elfeed-protocol
-        emacs-elfeed-score
-        emacs-emacsql
         emacs-embark
         emacs-engrave-faces
-        emacs-eval-in-repl-geiser
         emacs-exec-path-from-shell
         emacs-f
         emacs-flycheck
@@ -68,41 +39,26 @@
         emacs-flycheck-inline
         emacs-flycheck-package
         emacs-flycheck-rust
-        emacs-focus
         emacs-geiser
         emacs-geiser-guile
-        emacs-geiser-racket
         emacs-gnuplot
         emacs-god-mode
         emacs-gptel
         emacs-guix
         emacs-haskell-mode
         emacs-helpful
-        emacs-ht
-        emacs-hydra
-        emacs-hyperspace
-        emacs-iedit
         emacs-jit-spell
-        emacs-jedi
         emacs-json-mode
         emacs-julia-mode
         emacs-kind-icon
-        emacs-lispy
-        emacs-logview
         emacs-lsp-mode
-        emacs-lsp-docker
-        emacs-lsp-haskell
-        emacs-lsp-jedi
-        emacs-lsp-treemacs
         emacs-lsp-ui
         emacs-lua-mode
         emacs-magit
         emacs-marginalia
         emacs-markdown-mode
-        emacs-memory-usage
         emacs-mjolnir-mode
         emacs-modus-themes
-        emacs-multiple-cursors
         emacs-nerd-icons
         emacs-nerd-icons-completion
         emacs-nerd-icons-corfu
@@ -110,51 +66,28 @@
         emacs-nerd-icons-ibuffer
         emacs-no-littering
         emacs-oauth2
-        emacs-ob-racket
         emacs-olivetti
-        emacs-openwith
         emacs-orderless
         emacs-org-fragtog
-        emacs-org-modern
-        emacs-org-node
         emacs-org-roam
-        emacs-org-roam-bibtex
         emacs-org-roam-ui
-        emacs-org-super-agenda
-        emacs-paredit
         emacs-password-store
         emacs-pdf-tools
         emacs-pinentry
-        emacs-prescient
-        emacs-projectile
         emacs-rainbow-delimiters
         emacs-rainbow-mode
-        emacs-restart-emacs
-        emacs-rg
-        emacs-ripgrep
-        emacs-rustic
-        emacs-s
-        emacs-seq
+        emacs-reformatter
         emacs-setup
         emacs-simple-httpd
         emacs-smartparens
-        emacs-spinner
-        emacs-tablist
         emacs-toml-mode
-        emacs-transient-posframe
-        emacs-transmission
         emacs-treemacs
-        emacs-treemacs-extra
         emacs-vertico
-        emacs-vertico-posframe
         emacs-vterm
-        emacs-vterm-toggle
-        emacs-web-mode
         emacs-websocket
         emacs-which-key
         emacs-windsize
         emacs-xref
-        emacs-xref-union
         emacs-yaml-mode
         emacs-yasnippet))
 
