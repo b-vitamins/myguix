1604e4fc4169f32f81a50650668c5d32518d889e
Ayan Das <bvits@riseup.net>
2025-05-14 17:20:31 +0530

    packages: llvm-for-triton: Add package.

--- myguix/packages/llvm-pqrs.scm
+++ myguix/packages/llvm-pqrs.scm
@@ -1,32 +1,27 @@
 (define-module (myguix packages llvm-pqrs)
+  #:use-module (gnu packages gcc)
+  #:use-module (gnu packages version-control)
+  #:use-module (myguix packages nvidia)
   #:use-module (gnu packages llvm)
+  #:use-module (gnu packages maths)
+  #:use-module (gnu packages compression)
   #:use-module (guix gexp)
   #:use-module (guix packages)
   #:use-module (guix utils))
 
-(define-public llvm-with-bolt-17
+(define-public llvm-for-triton
   (package
-    (inherit llvm-17)
-    (name "llvm-with-bolt")
+    (inherit llvm-20)
+    (name "llvm")
     (arguments
-     (substitute-keyword-arguments (package-arguments llvm-17)
+     (substitute-keyword-arguments (package-arguments llvm-20)
        ((#:configure-flags flags)
         #~(append #$flags
-                  ;; Add BOLT support
-                  (list "-DLLVM_ENABLE_ASSERTIONS=ON"
-                        "-DLLVM_ENABLE_PROJECTS=bolt")))))))
-
-(define-public llvm-with-bolt-18
-  (package
-    (inherit llvm-18)
-    (name "llvm-with-bolt")
-    (arguments
-     (substitute-keyword-arguments (package-arguments llvm-18)
-       ((#:configure-flags flags)
-        #~(append #$flags
-                  ;; Add BOLT support
-                  (list "-DLLVM_ENABLE_ASSERTIONS=ON"
-                        "-DLLVM_ENABLE_PROJECTS=bolt")))))))
-
-(define-public llvm-with-bolt
-  llvm-with-bolt-18)
+                  ;; ───────── extra sub-projects ─────────
+                  (list "-DLLVM_ENABLE_PROJECTS=clang;lld;mlir"
+                        "-DLLVM_TARGETS_TO_BUILD=X86;NVPTX;AMDGPU"
+                        "-DLLVM_ENABLE_RTTI=ON")))))
+    (native-inputs (modify-inputs (package-native-inputs llvm-20)
+                     (prepend isl z3 zstd git-minimal cuda-toolkit)))
+    (inputs (modify-inputs (package-inputs llvm-20)
+              (prepend zstd)))))
