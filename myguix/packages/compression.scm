4cb5e2fe0daaf70eb74a805bf21423371037dd23
Ayan Das <bvits@riseup.net>
2025-05-28 19:39:03 +0530

    packages: miniz-for-pytorch: Add package.

--- myguix/packages/compression.scm
+++ myguix/packages/compression.scm
@@ -2,10 +2,14 @@
 ;;; Copyright © 2019 Pierre Neidhardt <mail@ambrevar.xyz>
 
 (define-module (myguix packages compression)
-  #:use-module (guix packages)
+  #:use-module (gnu packages compression)
   #:use-module (guix build-system gnu)
   #:use-module (guix download)
-  #:use-module (myguix licenses))
+  #:use-module (guix git-download)
+  #:use-module (guix packages)
+  #:use-module (guix utils)
+  #:use-module (myguix licenses)
+  #:use-module (srfi srfi-26))
 
 (define-public unrar
   (package
@@ -30,3 +34,30 @@
     (description "The RAR decompression program.  It is nonfree (as in
 freedom), but open-source.")
     (license (nonfree "https://www.win-rar.com/gtb_priv.html?&L=0"))))
+
+(define-public miniz-for-pytorch
+  (package
+    (inherit miniz)
+    (version "pytorch-2.7.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/richgel999/miniz")
+             (commit "3.0.2")))
+       (file-name (git-file-name (package-name miniz) version))
+       (sha256
+        (base32 "0672q35vjrpakmsr1gwj9k5fwv5ihzhahm19bq4y74wqpn91p7fw"))
+       (patches (map (lambda (patch)
+                       (search-path (map (cut string-append <>
+                                              "/myguix/patches") %load-path)
+                                    patch))
+                     '("miniz-for-pytorch-2.7.0.patch")))))
+    (arguments
+     (substitute-keyword-arguments (package-arguments miniz)
+       ((#:configure-flags flags
+         '())
+        ;; The changes break the examples.
+        `(cons "-DBUILD_EXAMPLES=OFF"
+               ,flags))))
+    (properties '((hidden? . #t)))))
