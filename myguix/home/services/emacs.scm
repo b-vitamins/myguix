a72086ac6eb4129315810e11e40cfc92a22e1b30
Ayan Das <bvits@riseup.net>
2025-06-07 22:39:24 +0530

    Sync channel files from development
    Synced from development branch commit: ea795ba2105a8710959a93824411e34899491721
    Files synced: myguix

--- myguix/home/services/emacs.scm
+++ myguix/home/services/emacs.scm
@@ -10,7 +10,7 @@
   #:export (my-home-emacs-service-type))
 
 (define (my-home-emacs-profile-service-type config)
-  (list emacs-next-pgtk
+  (list emacs-next
         emacs-ace-window
         emacs-adaptive-wrap
         emacs-all-the-icons
