329c7a069b55b9baac2e13274aa4f599fe48533f
Ayan Das <bvits@riseup.net>
2025-06-24 16:22:10 +0530

    packages: python-pytorch-lightning-cuda: Add package.

--- myguix/packages/machine-learning.scm
+++ myguix/packages/machine-learning.scm
@@ -3380,3 +3380,26 @@ features.")
      "Weights & Biases lets you track machine-learning experiments, version
 datasets and models, and collaborate with your team.")
     (license license:expat)))
+
+(define-public python-pytorch-lightning-cuda
+  (let ((commit "1617f70428a791b2d81c392d6a0b8a078d8e7fb1")
+        (revision "0"))
+    (package
+      (inherit python-pytorch-lightning)
+      (name "python-pytorch-lightning-cuda")
+      (version (git-version "2.5.2" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/Lightning-AI/pytorch-lightning")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "1i11n4094a8ysb7cj1lww23nh0mk3d9licw9c9pgzws2m0qy70yd"))))
+      (build-system pyproject-build-system)
+      (propagated-inputs (modify-inputs (package-propagated-inputs
+                                         python-pytorch-lightning)
+                           (replace "python-pytorch" python-pytorch-cuda)
+                           (replace "python-torchvision"
+                                    python-torchvision-cuda))))))
