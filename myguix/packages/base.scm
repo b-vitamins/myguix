d2ddf8fd40494bcde5c748a54aa0e658864228ca
Ayan Das <bvits@riseup.net>
2025-06-26 10:24:41 +0530

    packages: base: Add missing mail module import for mumi.

--- myguix/packages/base.scm
+++ myguix/packages/base.scm
@@ -32,6 +32,7 @@
   #:use-module (gnu packages kde)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages llvm)
+  #:use-module (gnu packages mail)
   #:use-module (gnu packages maths)
   #:use-module (gnu packages monitoring)
   #:use-module (gnu packages mp3)
