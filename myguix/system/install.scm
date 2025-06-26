2df6b934a26055e3691c6500bf157b5173a9956a
Ayan Das <bvits@riseup.net>
2025-06-26 09:57:49 +0530

    system: install: Update for new package bundle variables.

--- myguix/system/install.scm
+++ myguix/system/install.scm
@@ -46,16 +46,19 @@
     (initrd microcode-initrd)
     (keyboard-layout (keyboard-layout "us" "altgr-intl"
                                       #:options '("ctrl:nocaps")))
-    (packages (append %core-packages
-                      %versioning-packages
-                      %network-packages
-                      %basic-filesystem-packages
-                      %advanced-filesystem-packages
-                      %file-transfer-packages
-                      %general-fonts
-                      %cjk-fonts
-                      %unicode-fonts
-                      %google-fonts
+    (packages (append %core-minimal
+                      %core-extended
+                      %shell-modern
+                      %version-control
+                      %network-core
+                      %network-diagnostics
+                      %filesystem-core
+                      %filesystem-advanced
+                      %backup-tools
+                      %download-tools
+                      %compression-tools
+                      %fonts-essential
+                      %fonts-international
                       (operating-system-packages installation-os)))
     (services
      (modify-services (operating-system-user-services installation-os)
