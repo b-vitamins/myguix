b82c3ba4f80e775a76b366258f55e6737db60033
Ayan Das <bvits@riseup.net>
2025-08-24 01:56:44 +0530

    packages: linux: Update default kernel to 6.15.
    * myguix/packages/linux.scm (linux): Change from linux-6.14 to linux-6.15.

--- myguix/packages/linux.scm
+++ myguix/packages/linux.scm
@@ -200,7 +200,7 @@ on hardware which requires nonfree software to function."))))
   (corrupt-linux linux-libre-5.4))
 
 (define-public linux
-  linux-6.14)
+  linux-6.15)
 
 ;; linux-lts points to the *newest* released long-term support version.
 (define-public linux-lts
