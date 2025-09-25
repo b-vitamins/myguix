e92b204db5576d4a4b04938c9100e7c7546e5643
Ayan Das <bvits@riseup.net>
2025-09-25 16:20:43 +0530

    packages: llvm-pqrs: Pin llvm-for-triton to specific commit.
    * myguix/packages/llvm-pqrs.scm (llvm-for-triton): Change source to git
    checkout of commit 8957e64a20fc7f4277565c6cfe3e555c119783ce.  Add
    git-download and licenses imports.  Use git-version for versioning.

--- myguix/packages/llvm-pqrs.scm
+++ myguix/packages/llvm-pqrs.scm
@@ -7,21 +7,35 @@
   #:use-module (gnu packages compression)
   #:use-module (guix gexp)
   #:use-module (guix packages)
-  #:use-module (guix utils))
+  #:use-module (guix utils)
+  #:use-module (guix git-download)
+  #:use-module ((guix licenses) #:prefix license:))
 
 (define-public llvm-for-triton
-  (package
-    (inherit llvm-20)
-    (name "llvm")
-    (arguments
-     (substitute-keyword-arguments (package-arguments llvm-20)
-       ((#:configure-flags flags)
-        #~(append #$flags
-                  ;; ───────── extra sub-projects ─────────
-                  (list "-DLLVM_ENABLE_PROJECTS=clang;lld;mlir"
-                        "-DLLVM_TARGETS_TO_BUILD=X86;NVPTX;AMDGPU"
-                        "-DLLVM_ENABLE_RTTI=ON")))))
-    (native-inputs (modify-inputs (package-native-inputs llvm-20)
-                     (prepend isl z3 zstd git-minimal cuda-toolkit)))
-    (inputs (modify-inputs (package-inputs llvm-20)
-              (prepend zstd)))))
+  (let ((commit "8957e64a20fc7f4277565c6cfe3e555c119783ce")
+        (revision "1"))
+    (package
+      (inherit llvm-20)
+      (name "llvm-for-triton")
+      (version (git-version "20.0.0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/llvm/llvm-project")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "1540p24cdjg115frhn3yq375r5pfxjd87h8k8kvgwrn6y8f70dwn"))))
+      (arguments
+       (substitute-keyword-arguments (package-arguments llvm-20)
+         ((#:configure-flags flags)
+          #~(append #$flags
+                    ;; ───────── extra sub-projects ─────────
+                    (list "-DLLVM_ENABLE_PROJECTS=clang;lld;mlir"
+                          "-DLLVM_TARGETS_TO_BUILD=X86;NVPTX;AMDGPU"
+                          "-DLLVM_ENABLE_RTTI=ON")))))
+      (native-inputs (modify-inputs (package-native-inputs llvm-20)
+                       (prepend isl z3 zstd git-minimal cuda-toolkit)))
+      (inputs (modify-inputs (package-inputs llvm-20)
+                (prepend zstd))))))
