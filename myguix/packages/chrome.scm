3248d91131d59e31022758774459d00fa47c3b8e
Ayan Das <bvits@riseup.net>
2025-12-30 13:50:26 +0530

    packages: google-chrome-beta: Update to 143.0.7499.40.

--- myguix/packages/chrome.scm
+++ myguix/packages/chrome.scm
@@ -172,8 +172,8 @@
                       "119fdvnabicl387jy54p0r8bdr7lsv4in9fyyf6gmdg7dfsp27cf"))
 
 (define-public google-chrome-beta
-  (make-google-chrome "beta" "140.0.7339.16"
-                      "0zyq0sigr5pxhylp3mr45f4i3xjsbj2rmgvymgkb407ffmlcywn2"))
+  (make-google-chrome "beta" "143.0.7499.40"
+                      "0rzvn2xs0835nziamgghvl1mvaja4999r1df72llqcnll0bwwp1d"))
 
 (define-public google-chrome-unstable
   (make-google-chrome "unstable" "141.0.7354.0"
