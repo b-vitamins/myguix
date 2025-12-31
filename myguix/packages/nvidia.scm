71c97bc204f7e5b57638429b5edd67c1ddcd0f5f
Ayan Das <bvits@riseup.net>
2025-12-31 15:06:51 +0530

    packages: python-nvidia-ml-py: Disable tests.

--- myguix/packages/nvidia.scm
+++ myguix/packages/nvidia.scm
@@ -1124,6 +1124,7 @@ nvidia-smi.")
     (build-system python-build-system)
     (arguments
      (list
+      #:tests? #f      
       #:phases
       #~(modify-phases %standard-phases
           (add-after 'unpack 'fix-libnvidia
