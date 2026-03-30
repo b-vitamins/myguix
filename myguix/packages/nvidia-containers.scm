0190558a5bdeef2442bea3f16b282732c627524d
Ayan Das <bvits@riseup.net>
2026-03-30 16:17:00 +0530

    packages: Add libnvidia-container.

--- myguix/packages/nvidia-containers.scm
+++ myguix/packages/nvidia-containers.scm
@@ -1,11 +1,18 @@
 (define-module (myguix packages nvidia-containers)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages elf)
+  #:use-module (gnu packages golang)
   #:use-module (gnu packages linux)
+  #:use-module (gnu packages onc-rpc)
+  #:use-module (gnu packages pkg-config)
   #:use-module (guix build-system cargo)
+  #:use-module (guix build-system gnu)
   #:use-module (guix download)
   #:use-module (guix gexp)
   #:use-module ((guix licenses)
                 #:prefix license:)
   #:use-module (guix packages)
+  #:use-module (myguix packages nvidia)
   #:use-module ((myguix packages rust-crates-pqrs)
                 #:select (lookup-myguix-cargo-inputs)))
 
@@ -63,3 +70,140 @@
      "pathrs provides a Rust library and C-compatible API for performing
 safer path resolution operations on Linux.")
     (license (list license:mpl2.0 license:lgpl3+))))
+
+(define-public libnvidia-container
+  (package
+    (name "libnvidia-container")
+    (version "1.19.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://github.com/NVIDIA/libnvidia-container"
+                           "/archive/refs/tags/v" version ".tar.gz"))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "060p6kvq46pjbsdsihkz6hff5d4z9dngz3s4s6f7w17mpk9hmd11"))))
+    (build-system gnu-build-system)
+    (arguments
+     (list
+      #:tests? #f                  ;Upstream does not ship a standalone test suite.
+      #:make-flags
+      #~(let ((out #$output))
+          (list (string-append "LIB_VERSION=" #$version)
+                (string-append "REVISION=v" #$version)
+                "WITH_LIBELF=yes"
+                "WITH_TIRPC=yes"
+                "LDCONFIG=true"
+                (string-append "prefix=" out)
+                (string-append "exec_prefix=" out)
+                (string-append "bindir=" out "/bin")
+                (string-append "libdir=" out "/lib")
+                (string-append "docdir=" out "/share/doc")
+                (string-append "includedir=" out "/include")
+                (string-append "pkgconfdir=" out "/lib/pkgconfig")
+                (string-append "libdbgdir=" out "/lib/debug")
+                "CC=gcc"
+                (string-append
+                 "CPPFLAGS=-D_GNU_SOURCE -D_FORTIFY_SOURCE=2 -isystem "
+                 (assoc-ref %build-inputs "nvidia-modprobe") "/include"
+                 " -isystem "
+                 (assoc-ref %build-inputs "libtirpc") "/include/tirpc")
+                (string-append
+                 "GO=" (search-input-file %build-inputs "/bin/go"))))
+      #:phases
+      #~(modify-phases %standard-phases
+          (delete 'configure)
+          (add-after 'unpack 'patch-guix-paths
+            (lambda* (#:key inputs outputs #:allow-other-keys)
+              (let ((out (assoc-ref outputs "out"))
+                    (glibc (assoc-ref inputs "glibc"))
+                    (nvidia-modprobe (assoc-ref inputs "nvidia-modprobe")))
+                (setenv "HOME" (getcwd))
+                (mkdir-p ".cache/go-build")
+                (setenv "GOCACHE" (string-append (getcwd) "/.cache/go-build"))
+                (setenv "GOFLAGS" "-mod=vendor")
+                (substitute* "mk/common.mk"
+                  (("which \\$\\(CC\\)")
+                   "command -v $(CC)"))
+                (substitute* "mk/nvcgo.mk"
+                  (("\\$\\(RM\\) -rf \\$\\(SRCS_DIR\\)\n")
+                   "$(RM) -rf $(SRCS_DIR)\n\t$(MKDIR) -p $(dir $(SRCS_DIR))\n"))
+                (substitute* "Makefile"
+                  (("all: shared static tools")
+                   "all: shared tools")
+                  (("debug: shared static tools")
+                   "debug: shared tools")
+                  (("LIB_LDFLAGS        = -L\\$\\(DEPS_DIR\\)\\$\\(libdir\\) -shared -Wl,-soname=\\$\\(LIB_SONAME\\)")
+                   "LIB_LDFLAGS        = -shared -Wl,-soname=$(LIB_SONAME)")
+                  (("BIN_LDFLAGS        = -L\\. -pie \\$\\(LDFLAGS\\) -Wl,-rpath='\\$\\$ORIGIN/../\\$\\$LIB'")
+                   "BIN_LDFLAGS        = -L. -pie $(LDFLAGS) -Wl,-rpath=$(libdir)")
+                  (("LIB_LDLIBS_STATIC  = -l:libnvidia-modprobe-utils\\.a")
+                   (string-append
+                    "LIB_LDLIBS_STATIC  = "
+                    nvidia-modprobe "/lib/libnvidia-modprobe-utils.a"))
+                  (("LIB_LDLIBS_STATIC  \\+= -l:libtirpc\\.a")
+                   "LIB_LDLIBS_STATIC  += -ltirpc")
+                  (("\\$\\(MAKE\\) -f \\$\\(MAKE_DIR\\)/libtirpc\\.mk DESTDIR=\\$\\(DEPS_DIR\\) install")
+                   "true")
+                  (("\t\\$\\(MAKE\\) -f \\$\\(MAKE_DIR\\)/nvidia-modprobe\\.mk DESTDIR=\\$\\(DEPS_DIR\\) install\n")
+                   "")
+                  (("ifeq \\(\\$\\(WITH_LIBELF\\), no\\)\n\t\\$\\(MAKE\\) -f \\$\\(MAKE_DIR\\)/elftoolchain\\.mk DESTDIR=\\$\\(DEPS_DIR\\) install\nendif\n")
+                   "")
+                  (("\t\\$\\(INSTALL\\) -m 644 \\$\\(LIB_STATIC\\) \\$\\(DESTDIR\\)\\$\\(libdir\\)\n")
+                   "")
+                  (("ifeq \\(\\$\\(WITH_TIRPC\\), yes\\)\n\t\\$\\(MAKE\\) -f \\$\\(MAKE_DIR\\)/libtirpc\\.mk DESTDIR=\\$\\(DEPS_DIR\\) install\nendif\n")
+                   ""))
+                (substitute* "src/common.h"
+                  (("#define LIB_DIR\\s+\"/lib64\"")
+                   (string-append
+                    "#define LIB_DIR                   \""
+                    glibc "/lib\"")))
+                (substitute* "src/nvc_info.c"
+                  (("#include \"xfuncs.h\"\n")
+                   (string-append
+                    "#include \"xfuncs.h\"\n\n"
+                    "int nvidia_cap_get_device_file_attrs(const char *, int *, int *, char *);\n")))
+                (substitute* "src/nvc_internal.h"
+                  (("#define SONAME_LIBNVCGO \"libnvidia-container-go.so.1\"")
+                   (string-append
+                    "#define SONAME_LIBNVCGO \""
+                    out "/lib/libnvidia-container-go.so.1\""))))))
+          (add-after 'install 'fix-installed-soname-links
+            (lambda* (#:key outputs #:allow-other-keys)
+              (let ((libdir (string-append (assoc-ref outputs "out") "/lib")))
+                (with-directory-excursion libdir
+                  (symlink (string-append "libnvidia-container.so." #$version)
+                           "libnvidia-container.so.1")
+                  (delete-file "libnvidia-container.so")
+                  (symlink "libnvidia-container.so.1"
+                           "libnvidia-container.so")
+                  (symlink (string-append
+                            "libnvidia-container-go.so." #$version)
+                           "libnvidia-container-go.so.1")
+                  (delete-file "libnvidia-container-go.so")
+                  (symlink "libnvidia-container-go.so.1"
+                           "libnvidia-container-go.so")))))
+          (add-after 'fix-installed-soname-links 'remove-debug-artifacts
+            (lambda* (#:key outputs #:allow-other-keys)
+              (let ((debugdir (string-append
+                               (assoc-ref outputs "out") "/lib/debug")))
+                (when (file-exists? debugdir)
+                  (delete-file-recursively debugdir))))))))
+    (native-inputs
+     (list go
+           pkg-config
+           rpcsvc-proto))
+    (inputs
+     (list elfutils
+           glibc
+           libcap
+           libseccomp
+           libtirpc
+           nvidia-modprobe))
+    (home-page "https://github.com/NVIDIA/libnvidia-container")
+    (synopsis "NVIDIA container runtime library and CLI")
+    (description
+     "libnvidia-container provides the core library and command line tools
+used to configure Linux containers for access to NVIDIA GPUs.")
+    (license (list license:asl2.0
+                   license:bsd-3))))
