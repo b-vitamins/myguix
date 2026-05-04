e4a4fb8f83d90af719c7296613429460004d2cb4
Ayan Das <bvits@riseup.net>
2026-05-04 17:07:52 +0530

    services: authentication: Fix fprintd PAM integration.

--- myguix/services/authentication.scm
+++ myguix/services/authentication.scm
@@ -1,7 +1,9 @@
 (define-module (myguix services authentication)
   #:use-module (gnu)
+  #:use-module (gnu services base)
   #:use-module (gnu services dbus)
   #:use-module (gnu services configuration)
+  #:use-module (gnu system pam)
   #:export (fprintd-service-type))
 
 (define-configuration/no-serialization fprintd-configuration
@@ -26,50 +28,37 @@ This depends on your desktop configuration. For example, if you want GNOME promp
   (pam-entry (control "sufficient")
              (module fprintd-module)))
 
-(define (fprintd-unix-pam-entry)
-  (pam-entry (control "sufficient")
-             (module "pam_unix.so")
-             (arguments '("try_first_pass" "likeauth" "nullok"))))
+(define (fprintd-pam-transformer config fprintd-module)
+  (let ((service-names
+         (append (fprintd-configuration-unlock-other config)
+                 (if (fprintd-configuration-unlock-gdm? config)
+                     '("gdm-password")
+                     '()))))
+    (pam-extension
+     (transformer
+      (lambda (pam)
+        (if (member (pam-service-name pam) service-names)
+            (pam-service (inherit pam)
+                         (auth (cons (fprintd-pam-entry fprintd-module)
+                                     (pam-service-auth pam))))
+            pam))))))
 
-(define (fprintd-pam-other-services config fprintd-module)
-  (lambda (pam)
-    (if (member (pam-service-name pam)
-                (fprintd-configuration-unlock-other config))
-        (let ((fprintd-entry (fprintd-pam-entry fprintd-module))
-              (unix-entry (fprintd-unix-pam-entry)))
-          (pam-service (inherit pam)
-                       (auth (cons fprintd-entry
-                                   (cons unix-entry
-                                         (pam-service-auth pam)))))) pam)))
+(define (fprintd-gdm-fingerprint-pam-service fprintd-module)
+  ;; GDM may look for a dedicated "gdm-fingerprint" service.  Provide it
+  ;; explicitly, but transform existing GDM PAM services in place to avoid
+  ;; colliding with Guix's own gdm-password and gdm-launch-environment files.
+  (pam-service (inherit (unix-pam-service "gdm-fingerprint"
+                                          #:login-uid? #t))
+               (auth (list (pam-entry (control "required")
+                                      (module fprintd-module))))))
 
-(define (fprintd-pam-gdm-services fprintd-module)
-  (list
-   ;; Modify gdm-fingerprint, if used (this is less common)
-   (pam-service (inherit (unix-pam-service "gdm-fingerprint"
-                                           #:login-uid? #t))
-                (auth (list (pam-entry (control "required")
-                                       (module fprintd-module)))))
-   ;; Modify gdm-password for fingerprint authentication
-   (pam-service (inherit (unix-pam-service "gdm-password"
-                                           #:login-uid? #t))
-                (auth (list (fprintd-pam-entry fprintd-module)
-                            (fprintd-unix-pam-entry)
-                            (pam-entry (control "include")
-                                       (module "system-login")))))
-   ;; Optionally, modify gdm-launch-environment
-   (pam-service (inherit (unix-pam-service "gdm-launch-environment"))
-                (auth (list (fprintd-pam-entry fprintd-module)
-                            (fprintd-unix-pam-entry)
-                            (pam-entry (control "include")
-                                       (module "system-login")))))))
-
-(define (fprintd-pam-services config)
+(define (fprintd-pam-extensions config)
   (let ((fprintd-module (file-append (fprintd-configuration-fprintd config)
                                      "/lib/security/pam_fprintd.so")))
-    (cons (fprintd-pam-other-services config fprintd-module)
-          (if (fprintd-configuration-unlock-gdm? config)
-              (fprintd-pam-gdm-services fprintd-module)
-              '()))))
+    (append (list (fprintd-pam-transformer config fprintd-module))
+            (if (fprintd-configuration-unlock-gdm? config)
+                (list (fprintd-gdm-fingerprint-pam-service fprintd-module))
+                '()))))
 
 (define (fprintd-dbus-service config)
   (list (fprintd-configuration-fprintd config)))
@@ -81,8 +70,7 @@ This depends on your desktop configuration. For example, if you want GNOME promp
                                   (service-extension polkit-service-type
                                                      fprintd-dbus-service)
                                   (service-extension pam-root-service-type
-                                                     fprintd-pam-services)))
+                                                     fprintd-pam-extensions)))
                 (default-value (fprintd-configuration))
                 (description
                  "Run fprintd, a fingerprint management daemon, with optional support for unlocking GDM and other PAM services using fingerprint authentication.")))
-
