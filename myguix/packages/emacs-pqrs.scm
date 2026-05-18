67903ac214014c190a4ca8561df5a43bf8a47751
Ayan Das <bvits@riseup.net>
2026-05-18 12:27:35 +0530

    packages: emacs-refbox: Update to 0.6.2.

--- myguix/packages/emacs-pqrs.scm
+++ myguix/packages/emacs-pqrs.scm
@@ -756,7 +756,7 @@ both LaTeX and Org modes and support context-aware expansions.")
 (define-public refbox
   (package
     (name "refbox")
-    (version "0.6.1")
+    (version "0.6.2")
     (source
      (origin
        (method git-fetch)
@@ -765,7 +765,7 @@ both LaTeX and Org modes and support context-aware expansions.")
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "00y9ngravjdlck5c4vgimdhxf7vciwjfrrsfla6zi50cisd2kppj"))))
+        (base32 "07j2q2lv1dvbh1cvjl2i4b5a3dd84g08dyb318jrcfr72imxy2dl"))))
     (build-system cargo-build-system)
     (arguments
      (list
@@ -797,7 +797,7 @@ lookup, source-location, resource, and formatting operations over JSON-RPC.")
 (define-public emacs-refbox
   (package
     (name "emacs-refbox")
-    (version "0.6.1")
+    (version "0.6.2")
     (source
      (origin
        (method git-fetch)
@@ -806,7 +806,7 @@ lookup, source-location, resource, and formatting operations over JSON-RPC.")
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "00y9ngravjdlck5c4vgimdhxf7vciwjfrrsfla6zi50cisd2kppj"))))
+        (base32 "07j2q2lv1dvbh1cvjl2i4b5a3dd84g08dyb318jrcfr72imxy2dl"))))
     (build-system emacs-build-system)
     (arguments
      (list
