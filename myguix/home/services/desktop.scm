add47b9f14f541cece20e6e105256bf2862f84e3
Ayan Das <bvits@riseup.net>
2026-06-05 14:51:02 +0530

    home: desktop: Add MIME applications helpers.

--- myguix/home/services/desktop.scm
+++ myguix/home/services/desktop.scm
@@ -16,6 +16,10 @@
             %desktop-xdg-directories
             
             ;; MIME applications
+            mimeapps-list
+            mimeapps-associations
+            desktop-mimeapps-defaults
+            home-mimeapps-service
             %gnome-mimeapps
             %kde-mimeapps
             
@@ -63,71 +67,165 @@
 
 ;;; MIME applications
 
-(define-public %gnome-mimeapps
-  (plain-file "mimeapps.list"
-              "[Default Applications]
-text/html=firefox.desktop
-x-scheme-handler/http=firefox.desktop
-x-scheme-handler/https=firefox.desktop
-x-scheme-handler/about=firefox.desktop
-x-scheme-handler/unknown=firefox.desktop
-application/pdf=org.gnome.Evince.desktop
-image/png=org.gnome.Loupe.desktop
-image/jpeg=org.gnome.Loupe.desktop
-image/gif=org.gnome.Loupe.desktop
-image/webp=org.gnome.Loupe.desktop
-video/mp4=mpv.desktop
-video/x-matroska=mpv.desktop
-video/webm=mpv.desktop
-audio/mpeg=org.gnome.Rhythmbox3.desktop
-audio/flac=org.gnome.Rhythmbox3.desktop
-audio/mp3=org.gnome.Rhythmbox3.desktop
-text/plain=org.gnome.TextEditor.desktop
-text/x-c=org.gnome.TextEditor.desktop
-text/x-python=org.gnome.TextEditor.desktop
-application/x-shellscript=org.gnome.TextEditor.desktop
-inode/directory=org.gnome.Nautilus.desktop
+(define %browser-mime-types
+  '("text/html"
+    "x-scheme-handler/http"
+    "x-scheme-handler/https"
+    "x-scheme-handler/about"
+    "x-scheme-handler/unknown"))
 
-[Added Associations]
-text/html=firefox.desktop;
-x-scheme-handler/http=firefox.desktop;
-x-scheme-handler/https=firefox.desktop;
-application/pdf=org.gnome.Evince.desktop;
-image/png=org.gnome.Loupe.desktop;
-image/jpeg=org.gnome.Loupe.desktop;"))
+(define %image-mime-types
+  '("image/png"
+    "image/jpeg"
+    "image/gif"
+    "image/webp"))
+
+(define %video-mime-types
+  '("video/mp4"
+    "video/x-matroska"
+    "video/webm"))
+
+(define %audio-mime-types
+  '("audio/mpeg"
+    "audio/flac"
+    "audio/mp3"))
+
+(define %text-mime-types
+  '("text/plain"
+    "text/x-c"
+    "text/x-python"
+    "application/x-shellscript"))
+
+(define (mimeapps-associations desktop-entry mime-types)
+  "Return MIME association pairs mapping MIME-TYPES to DESKTOP-ENTRY."
+  (map (lambda (mime-type)
+         (cons mime-type desktop-entry))
+       mime-types))
+
+(define (maybe-mimeapps-associations desktop-entry mime-types)
+  (if desktop-entry
+      (mimeapps-associations desktop-entry mime-types)
+      '()))
+
+(define* (mimeapps-entry-value value #:key (terminal-semicolon? #f))
+  (string-append
+   (string-join (if (list? value) value (list value)) ";")
+   (if terminal-semicolon? ";" "")))
+
+(define* (mimeapps-section title entries #:key (terminal-semicolon? #f))
+  (if (null? entries)
+      ""
+      (string-append
+       "[" title "]\n"
+       (apply string-append
+              (map (lambda (entry)
+                     (string-append (car entry)
+                                    "="
+                                    (mimeapps-entry-value
+                                     (cdr entry)
+                                     #:terminal-semicolon? terminal-semicolon?)
+                                    "\n"))
+                   entries))
+       "\n")))
+
+(define* (mimeapps-list #:key
+                        (name "mimeapps.list")
+                        (defaults '())
+                        (added-associations '())
+                        (removed-associations '()))
+  "Return a @file{mimeapps.list} file for the supplied XDG associations."
+  (plain-file name
+              (string-append
+               (mimeapps-section "Default Applications" defaults)
+               (mimeapps-section "Added Associations" added-associations
+                                 #:terminal-semicolon? #t)
+               (mimeapps-section "Removed Associations" removed-associations
+                                 #:terminal-semicolon? #t))))
+
+(define* (desktop-mimeapps-defaults #:key
+                                    (browser #f)
+                                    (pdf-viewer #f)
+                                    (image-viewer #f)
+                                    (video-player #f)
+                                    (audio-player #f)
+                                    (text-editor #f)
+                                    (file-manager #f)
+                                    (extra-defaults '()))
+  "Return common desktop MIME defaults using caller-supplied desktop entries."
+  (append
+   (maybe-mimeapps-associations browser %browser-mime-types)
+   (if pdf-viewer
+       `(("application/pdf" . ,pdf-viewer))
+       '())
+   (maybe-mimeapps-associations image-viewer %image-mime-types)
+   (maybe-mimeapps-associations video-player %video-mime-types)
+   (maybe-mimeapps-associations audio-player %audio-mime-types)
+   (maybe-mimeapps-associations text-editor %text-mime-types)
+   (if file-manager
+       `(("inode/directory" . ,file-manager))
+       '())
+   extra-defaults))
+
+(define* (home-mimeapps-service #:key
+                                (service-name 'mimeapps)
+                                (defaults '())
+                                (added-associations '())
+                                (removed-associations '())
+                                (file #f))
+  "Return a Home service installing @file{mimeapps.list}.
+
+When FILE is provided it is installed directly.  Otherwise it is generated
+from DEFAULTS, ADDED-ASSOCIATIONS, and REMOVED-ASSOCIATIONS."
+  (simple-service service-name
+                  home-xdg-configuration-files-service-type
+                  `(("mimeapps.list"
+                     ,(or file
+                          (mimeapps-list
+                           #:defaults defaults
+                           #:added-associations added-associations
+                           #:removed-associations removed-associations))))))
+
+(define-public %gnome-mimeapps
+  (mimeapps-list
+   #:defaults
+   (desktop-mimeapps-defaults
+    #:browser "firefox.desktop"
+    #:pdf-viewer "org.gnome.Evince.desktop"
+    #:image-viewer "org.gnome.Loupe.desktop"
+    #:video-player "mpv.desktop"
+    #:audio-player "org.gnome.Rhythmbox3.desktop"
+    #:text-editor "org.gnome.TextEditor.desktop"
+    #:file-manager "org.gnome.Nautilus.desktop")
+   #:added-associations
+   (append
+    (mimeapps-associations "firefox.desktop"
+                           '("text/html"
+                             "x-scheme-handler/http"
+                             "x-scheme-handler/https"))
+    '(("application/pdf" . "org.gnome.Evince.desktop")
+      ("image/png" . "org.gnome.Loupe.desktop")
+      ("image/jpeg" . "org.gnome.Loupe.desktop")))))
 
 (define-public %kde-mimeapps
-  (plain-file "mimeapps.list"
-              "[Default Applications]
-text/html=firefox.desktop
-x-scheme-handler/http=firefox.desktop
-x-scheme-handler/https=firefox.desktop
-x-scheme-handler/about=firefox.desktop
-x-scheme-handler/unknown=firefox.desktop
-application/pdf=okular.desktop
-image/png=gwenview.desktop
-image/jpeg=gwenview.desktop
-image/gif=gwenview.desktop
-image/webp=gwenview.desktop
-video/mp4=mpv.desktop
-video/x-matroska=mpv.desktop
-video/webm=mpv.desktop
-audio/mpeg=elisa.desktop
-audio/flac=elisa.desktop
-audio/mp3=elisa.desktop
-text/plain=kate.desktop
-text/x-c=kate.desktop
-text/x-python=kate.desktop
-application/x-shellscript=kate.desktop
-inode/directory=dolphin.desktop
-
-[Added Associations]
-text/html=firefox.desktop;
-x-scheme-handler/http=firefox.desktop;
-x-scheme-handler/https=firefox.desktop;
-application/pdf=okular.desktop;
-image/png=gwenview.desktop;
-image/jpeg=gwenview.desktop;"))
+  (mimeapps-list
+   #:defaults
+   (desktop-mimeapps-defaults
+    #:browser "firefox.desktop"
+    #:pdf-viewer "okular.desktop"
+    #:image-viewer "gwenview.desktop"
+    #:video-player "mpv.desktop"
+    #:audio-player "elisa.desktop"
+    #:text-editor "kate.desktop"
+    #:file-manager "dolphin.desktop")
+   #:added-associations
+   (append
+    (mimeapps-associations "firefox.desktop"
+                           '("text/html"
+                             "x-scheme-handler/http"
+                             "x-scheme-handler/https"))
+    '(("application/pdf" . "okular.desktop")
+      ("image/png" . "gwenview.desktop")
+      ("image/jpeg" . "gwenview.desktop")))))
 
 
 ;;; Desktop home services
