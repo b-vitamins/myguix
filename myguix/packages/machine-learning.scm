1123066113b0ddf4285c39842956bcae414066f5
Ayan Das <bvits@riseup.net>
2026-06-05 11:10:33 +0530

    packages: python-optax: Update to 0.2.8.

--- myguix/packages/machine-learning.scm
+++ myguix/packages/machine-learning.scm
@@ -2867,14 +2867,13 @@ reliable JAX code.  This includes utils to help:
 (define-public python-optax
   (package
     (name "python-optax")
-    ;; 0.1.6 needs a more recent numpy
-    (version "0.1.5")
+    (version "0.2.8")
     (source
      (origin
        (method url-fetch)
        (uri (pypi-uri "optax" version))
        (sha256
-        (base32 "0bhgaaxvqli3b2081zp9ycb2c2hqba0fcpigaqjxbnsidyspk8qa"))))
+        (base32 "1c7h1asl70qzk040jqc0lk8mfk9v2ysg366pljxfxhvg0qsmn8jv"))))
     (build-system pyproject-build-system)
     ;; Tests require haiku, tensorflow, and flax, but flax needs
     ;; optax.
@@ -2883,8 +2882,7 @@ reliable JAX code.  This includes utils to help:
       #:tests? #f))
     (propagated-inputs (list python-absl-py python-chex python-jax
                              python-jaxlib python-numpy))
-    (native-inputs (list python-dm-tree python-pytest python-setuptools
-                         python-wheel))
+    (native-inputs (list python-dm-tree python-flit-core python-pytest))
     (home-page "https://github.com/google-deepmind/optax/")
     (synopsis "Gradient processing and optimization library for JAX")
     (description "Optax is a gradient processing and optimization
